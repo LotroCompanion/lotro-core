c0d35eb5ff091a9263f808f0f018f1b087226fa5
Damien Morcellet <dmorcellet@free.fr>
2016-02-15 20:37:54 +0000

    delta4j/lotro-core:     * added support rating curves

---
+++ src/test/java/delta/games/lotro/character/stats/TestRatingCurve.java
@@ -0,0 +1,24 @@
+package delta.games.lotro.character.stats;
+
+/**
+ * Test class for rating curves.
+ * @author DAM
+ */
+public class TestRatingCurve
+{
+  /**
+   * Main method for this test.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    // Critical hit / critical rating
+    double[][] values= { {15.0,1190.0/3,70}, {5,794.8,794.8/19}, {5,1075.2,1075.2/19}};
+    RatingCurve curve=new RatingCurve(values);
+    System.out.println(curve.getPercentage(10000,100));
+    System.out.println(curve.getPercentage(7000,100));
+    System.out.println(curve.getPercentage(11183,100));
+    System.out.println(curve.getPercentage(7000+794.8/19*100,100));
+    System.out.println(curve.getPercentage(16843,100));
+  }
+}
