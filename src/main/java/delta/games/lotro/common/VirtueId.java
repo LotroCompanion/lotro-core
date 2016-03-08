054f7c53dad7d46631d847b119b6b45be9048700
Damien Morcellet <dmorcellet@free.fr>
2016-03-08 21:12:20 +0000

    delta4j/lotro-core:     * added virtue identifiers

---
+++ src/main/java/delta/games/lotro/common/VirtueId.java
@@ -0,0 +1,88 @@
+package delta.games.lotro.common;
+
+/**
+ * Virtue identifiers.
+ * @author DAM
+ */
+public enum VirtueId {
+  /**
+   * Charity.
+   */
+  CHARITY,
+  /**
+   * Compassion.
+   */
+  COMPASSION,
+  /**
+   * Confidence.
+   */
+  CONFIDENCE,
+  /**
+   * Determination.
+   */
+  DETERMINATION,
+  /**
+   * Discipline.
+   */
+  DISCIPLINE,
+  /**
+   * Empathy.
+   */
+  EMPATHY,
+  /**
+   * Fidelity.
+   */
+  FIDELITY,
+  /**
+   * Fortitude.
+   */
+  FORTITUDE,
+  /**
+   * Hosnesty.
+   */
+  HONESTY,
+  /**
+   * Honour.
+   */
+  HONOUR,
+  /**
+   * Idealism.
+   */
+  IDEALISM,
+  /**
+   * Innocence.
+   */
+  INNOCENCE,
+  /**
+   * Justice.
+   */
+  JUSTICE,
+  /**
+   * Loyalty.
+   */
+  LOYALTY,
+  /**
+   * Mercy.
+   */
+  MERCY,
+  /**
+   * Patience.
+   */
+  PATIENCE,
+  /**
+   * Tolerance.
+   */
+  TOLERANCE,
+  /**
+   * Valour.
+   */
+  VALOUR,
+  /**
+   * Wisdom.
+   */
+  WISDOM,
+  /**
+   * Zeal.
+   */
+  ZEAL
+}
