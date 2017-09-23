918d97f26fbefca0e9fa3063e0a8961faabb3127
Damien Morcellet <dmorcellet@free.fr>
2017-09-23 20:05:35 +0200

    Updated buff 'Balance of Man' with formulas of Update 21

--- src/main/java/delta/games/lotro/character/stats/buffs/BalanceOfMan.java
+++ src/main/java/delta/games/lotro/character/stats/buffs/BalanceOfMan.java
@@ -22,10 +22,33 @@ public class BalanceOfMan extends AbstractBuffImpl
     // 105: 848
     // 106: 1696
     // 107: 1880
-    // TODO find out formula
+    // 108: 2070?
+    // 109: 2260
+    // 110: 2440?
+    // 111: 2630?
+    // 112: 2820?
+    // 113: 3010?
+    // 114: 3200?
+    // 115: 3392?
+    // Giseldah:
+    // For lvl106-115, Balance of Man and Shield Brawler follow
+    // the 2-4x lvl105 scheme with rounded to a precision of 10 for in
+    // between levels 107-114 (last digit always 0).
+
     if (level<=105) value=8.08f*level;
-    else if (level==106) value=1696;
-    else /*if (level==107)*/ value=1880;
+    else if ((level>=106) && (level<=115))
+    {
+      float factor=2+((level-106)*2.0f)/9;
+      value=848*factor;
+      if ((level>=107) && (level<=114))
+      {
+        value=10*(int)(value/10);
+      }
+    }
+    else
+    {
+      value=0;
+    }
     for(STAT stat : TARGETS)
     {
       stats.addStat(stat,new FixedDecimalsInteger(value));
