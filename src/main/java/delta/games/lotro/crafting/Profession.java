6aca599b9271ca67870e0d611d927cc58ecd957e
Damien Morcellet <dmorcellet@free.fr>
2017-10-20 15:38:48 +0200

    Added method Profession.hasGuild

--- src/main/java/delta/games/lotro/crafting/Profession.java
+++ src/main/java/delta/games/lotro/crafting/Profession.java
@@ -15,57 +15,60 @@ public class Profession
   private static HashMap<String,Profession> _instancesByKey=new HashMap<String,Profession>();
   private String _key;
   private String _label;
+  private boolean _hasGuild;
 
   /**
    * Prospector.
    */
-  public static final Profession PROSPECTOR=new Profession("PROSPECTOR","Prospector");
+  public static final Profession PROSPECTOR=new Profession("PROSPECTOR","Prospector",false);
   /**
    * Forester.
    */
-  public static final Profession FORESTER=new Profession("FORESTER","Forester");
+  public static final Profession FORESTER=new Profession("FORESTER","Forester",false);
   /**
    * Farmer.
    */
-  public static final Profession FARMER=new Profession("FARMER","Farmer");
+  public static final Profession FARMER=new Profession("FARMER","Farmer",false);
   /**
    * Metalsmith.
    */
-  public static final Profession METALSMITH=new Profession("METALSMITH","Metalsmith");
+  public static final Profession METALSMITH=new Profession("METALSMITH","Metalsmith",true);
   /**
    * Tailor.
    */
-  public static final Profession TAILOR=new Profession("TAILOR","Tailor");
+  public static final Profession TAILOR=new Profession("TAILOR","Tailor",true);
   /**
    * Weaponsmith.
    */
-  public static final Profession WEAPONSMITH=new Profession("WEAPONSMITH","Weaponsmith");
+  public static final Profession WEAPONSMITH=new Profession("WEAPONSMITH","Weaponsmith",true);
   /**
    * Woodworker.
    */
-  public static final Profession WOODWORKER=new Profession("WOODWORKER","Woodworker");
+  public static final Profession WOODWORKER=new Profession("WOODWORKER","Woodworker",true);
   /**
    * Scholar.
    */
-  public static final Profession SCHOLAR=new Profession("SCHOLAR","Scholar");
+  public static final Profession SCHOLAR=new Profession("SCHOLAR","Scholar",true);
   /**
    * Cook.
    */
-  public static final Profession COOK=new Profession("COOK","Cook");
+  public static final Profession COOK=new Profession("COOK","Cook",true);
   /**
    * Jeweller.
    */
-  public static final Profession JEWELLER=new Profession("JEWELLER","Jeweller");
+  public static final Profession JEWELLER=new Profession("JEWELLER","Jeweller",true);
 
   /**
    * Constructor.
    * @param key Internal key.
    * @param label Displayable label.
+   * @param hasGuild Indicates if this profession has a guild or not.
    */
-  public Profession(String key, String label)
+  public Profession(String key, String label, boolean hasGuild)
   {
     _key=key;
     _label=label;
+    _hasGuild=hasGuild;
     _instances.put(label,this);
     _instances.put(key,this);
     _instancesByKey.put(key,this);
@@ -87,6 +90,15 @@ public class Profession
   public String getLabel()
   {
     return _label;
+  }
+
+  /**
+   * Indicates if this profession has a guild or not.
+   * @return <code>true</code> if it has a guild, <code>false</code> otherwise.
+   */
+  public boolean hasGuild()
+  {
+    return _hasGuild;
   }
 
   /**
