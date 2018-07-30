1eeffdff80889773a0ad6733728db2cef96e45c1
Damien Morcellet <dmorcellet@free.fr>
2018-07-30 22:57:22 +0200

    Re-introduced CountedItem class as base class for StoredItem

--- src/main/java/delta/games/lotro/character/storage/StoredItem.java
+++ src/main/java/delta/games/lotro/character/storage/StoredItem.java
@@ -1,17 +1,14 @@
 package delta.games.lotro.character.storage;
 
-import delta.games.lotro.lore.items.Item;
+import delta.games.lotro.lore.items.CountedItem;
 import delta.games.lotro.lore.items.ItemProxy;
 
 /**
  * Stored item.
  * @author DAM
  */
-public class StoredItem
+public class StoredItem extends CountedItem
 {
-  private ItemProxy _proxy;
-  private int _quantity;
-
   /**
    * Constructor.
    * @param itemProxy Item proxy.
@@ -19,61 +16,9 @@ public class StoredItem
    */
   public StoredItem(ItemProxy itemProxy, int quantity)
   {
-    _proxy=itemProxy;
-    _quantity=quantity;
+    super(itemProxy,quantity);
   }
 
-  /**
-   * Get the name of the item.
-   * @return an item name.
-   */
-  public String getName()
-  {
-    return _proxy.getName();
-  }
-
-  /**
-   * Get the item quantity.
-   * @return a quantity.
-   */
-  public int getQuantity()
-  {
-    return _quantity;
-  }
-
-  /**
-   * Add a quantity of this item.
-   * @param quantity Quantity to add.
-   */
-  public void add(int quantity)
-  {
-    _quantity+=quantity;
-  }
-
-  /**
-   * Get the icon.
-   * @return an icon or <code>null</code> if not set.
-   */
-  public String getIcon()
-  {
-    return _proxy.getIcon();
-  }
-
-  /**
-   * Get the managed item.
-   * @return an item.
-   */
-  public Item getItem()
-  {
-    return _proxy.getItem();
-  }
-
-  @Override
-  public String toString()
-  {
-    StringBuilder sb=new StringBuilder();
-    sb.append(_quantity).append(' ');
-    sb.append(_proxy);
-    return sb.toString();
-  }
+  // TODO add location
+  // TODO add owner
 }
---
+++ src/main/java/delta/games/lotro/lore/items/CountedItem.java
@@ -0,0 +1,79 @@
+package delta.games.lotro.lore.items;
+
+import delta.games.lotro.lore.items.Item;
+import delta.games.lotro.lore.items.ItemProxy;
+
+/**
+ * Item and count.
+ * @author DAM
+ */
+public class CountedItem
+{
+  private ItemProxy _proxy;
+  private int _quantity;
+
+  /**
+   * Constructor.
+   * @param itemProxy Item proxy.
+   * @param quantity Quantity.
+   */
+  public CountedItem(ItemProxy itemProxy, int quantity)
+  {
+    _proxy=itemProxy;
+    _quantity=quantity;
+  }
+
+  /**
+   * Get the name of the item.
+   * @return an item name.
+   */
+  public String getName()
+  {
+    return _proxy.getName();
+  }
+
+  /**
+   * Get the item quantity.
+   * @return a quantity.
+   */
+  public int getQuantity()
+  {
+    return _quantity;
+  }
+
+  /**
+   * Add a quantity of this item.
+   * @param quantity Quantity to add.
+   */
+  public void add(int quantity)
+  {
+    _quantity+=quantity;
+  }
+
+  /**
+   * Get the icon.
+   * @return an icon or <code>null</code> if not set.
+   */
+  public String getIcon()
+  {
+    return _proxy.getIcon();
+  }
+
+  /**
+   * Get the managed item.
+   * @return an item.
+   */
+  public Item getItem()
+  {
+    return _proxy.getItem();
+  }
+
+  @Override
+  public String toString()
+  {
+    StringBuilder sb=new StringBuilder();
+    sb.append(_quantity).append(' ');
+    sb.append(_proxy);
+    return sb.toString();
+  }
+}
