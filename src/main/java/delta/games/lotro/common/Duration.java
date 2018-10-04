7aabd4a71d4907ed05712e8b1be47ddaf21d71ae
Damien Morcellet <dmorcellet@free.fr>
2018-10-04 20:47:01 +0200

    Added a method to parse duration strings

--- src/main/java/delta/games/lotro/common/Duration.java
+++ src/main/java/delta/games/lotro/common/Duration.java
@@ -1,5 +1,7 @@
 package delta.games.lotro.common;
 
+import delta.common.utils.NumericTools;
+
 /**
  * Duration related tools.
  * @author DAM
@@ -50,5 +52,52 @@ public class Duration
       sb.append(seconds).append('s');
     }
     return sb.toString();
+  }
+
+  /**
+   * Parse a duration string (DdHhMmSs).
+   * @param input Input string.
+   * @return A duration value or <code>null</code> if parsing failed.
+   */
+  public static Integer parseDurationString(String input)
+  {
+    int days=0;
+    int hours=0;
+    int minutes=0;
+    int seconds=0;
+    input=input.replace(" ","");
+    // Days
+    int daysIndex=input.indexOf('d');
+    if (daysIndex!=-1)
+    {
+      days=NumericTools.parseInt(input.substring(0,daysIndex),-1);
+      if (days==-1) return null;
+      input=input.substring(daysIndex+1);
+    }
+    // Hours
+    int hoursIndex=input.indexOf('h');
+    if (hoursIndex!=-1)
+    {
+      hours=NumericTools.parseInt(input.substring(0,hoursIndex),-1);
+      if (hours==-1) return null;
+      input=input.substring(hoursIndex+1);
+    }
+    // Minutes
+    int minutesIndex=input.indexOf('m');
+    if (minutesIndex!=-1)
+    {
+      minutes=NumericTools.parseInt(input.substring(0,minutesIndex),-1);
+      if (minutes==-1) return null;
+      input=input.substring(minutesIndex+1);
+    }
+    // Seconds
+    int secondsIndex=input.indexOf('s');
+    if (secondsIndex!=-1)
+    {
+      seconds=NumericTools.parseInt(input.substring(0,secondsIndex),-1);
+      if (seconds==-1) return null;
+      input=input.substring(secondsIndex+1);
+    }
+    return Integer.valueOf(days*DAY+hours*HOUR+minutes*MINUTE+seconds);
   }
 }
