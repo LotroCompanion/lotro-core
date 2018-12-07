b6bb7ad9709e12446b3ba94b5445b77434888c53
Damien Morcellet <dmorcellet@free.fr>
2018-12-07 17:53:41 +0100

    Handle operators MULTIPLY and SUBSTRACT in trait based buffs

--- src/main/java/delta/games/lotro/character/stats/buffs/TraitBuff.java
+++ src/main/java/delta/games/lotro/character/stats/buffs/TraitBuff.java
@@ -8,6 +8,7 @@ import delta.games.lotro.character.stats.BasicStatsSet;
 import delta.games.lotro.character.stats.STAT;
 import delta.games.lotro.character.traits.TraitDescription;
 import delta.games.lotro.common.stats.ScalableStatProvider;
+import delta.games.lotro.common.stats.StatOperator;
 import delta.games.lotro.common.stats.StatProvider;
 import delta.games.lotro.common.stats.StatsProvider;
 import delta.games.lotro.common.stats.TieredScalableStatProvider;
@@ -33,41 +34,27 @@ public class TraitBuff extends AbstractBuffImpl
   @Override
   public BasicStatsSet getStats(CharacterData character, BuffInstance buff)
   {
-    Integer tier=buff.getTier();
     StatsProvider statsProvider=_trait.getStatsProvider();
-    int level=character.getLevel();
-    int nbTiers=_trait.getTiersCount();
     BasicStatsSet stats=new BasicStatsSet();
     int nbStats=statsProvider.getNumberOfStatProviders();
     for(int i=0;i<nbStats;i++)
     {
       StatProvider provider=statsProvider.getStatProvider(i);
-      int tierValue=(tier!=null)?tier.intValue():1;
-      Float value=null;
-      if (provider instanceof TieredScalableStatProvider)
+      StatOperator operator=provider.getOperator();
+      if (operator==StatOperator.MULTIPLY)
       {
-        value=provider.getStatValue(tierValue,level);
+        continue;
       }
-      else if (provider instanceof ScalableStatProvider)
-      {
-        ScalableStatProvider scalableStatProvider=(ScalableStatProvider)provider;
-        if (nbTiers>1)
-        {
-          value=scalableStatProvider.getStatValue(1,tierValue);
-        }
-        else
-        {
-          value=scalableStatProvider.getStatValue(1,level);
-        }
-      }
-      else
-      {
-        value=provider.getStatValue(1,level);
-      }
+      Float value=getStatValue(character,buff,provider);
       if (value!=null)
       {
+        float statValue=value.floatValue();
+        if (operator==StatOperator.SUBSTRACT)
+        {
+          statValue=-statValue;
+        }
         STAT stat=provider.getStat();
-        stats.setStat(stat,new FixedDecimalsInteger(value.floatValue()));
+        stats.setStat(stat,new FixedDecimalsInteger(statValue));
       }
     }
     return stats;
@@ -76,7 +63,30 @@ public class TraitBuff extends AbstractBuffImpl
   @Override
   public BasicStatsSet getStats(CharacterData character, BasicStatsSet raw, BuffInstance buff)
   {
-    return null;
+    BasicStatsSet stats=null;
+    StatsProvider statsProvider=_trait.getStatsProvider();
+    int nbStats=statsProvider.getNumberOfStatProviders();
+    for(int i=0;i<nbStats;i++)
+    {
+      StatProvider provider=statsProvider.getStatProvider(i);
+      StatOperator operator=provider.getOperator();
+      if (operator==StatOperator.MULTIPLY)
+      {
+        Float value=getStatValue(character,buff,provider);
+        if (value!=null)
+        {
+          STAT stat=provider.getStat();
+          FixedDecimalsInteger rawStat=raw.getStat(stat);
+          FixedDecimalsInteger contrib=new FixedDecimalsInteger(rawStat.floatValue()*(value.floatValue()-1));
+          if (stats==null)
+          {
+            stats=new BasicStatsSet();
+          }
+          stats.setStat(stat,contrib);
+        }
+      }
+    }
+    return stats;
   }
 
   @Override
@@ -92,5 +102,35 @@ public class TraitBuff extends AbstractBuffImpl
       }
     }
     return ret;
+  }
+
+  private Float getStatValue(CharacterData character, BuffInstance buff, StatProvider provider)
+  {
+    Integer tier=buff.getTier();
+    int tierValue=(tier!=null)?tier.intValue():1;
+    int level=character.getLevel();
+    Float value=null;
+    if (provider instanceof TieredScalableStatProvider)
+    {
+      value=provider.getStatValue(tierValue,level);
+    }
+    else if (provider instanceof ScalableStatProvider)
+    {
+      ScalableStatProvider scalableStatProvider=(ScalableStatProvider)provider;
+      int nbTiers=_trait.getTiersCount();
+      if (nbTiers>1)
+      {
+        value=scalableStatProvider.getStatValue(1,tierValue);
+      }
+      else
+      {
+        value=scalableStatProvider.getStatValue(1,level);
+      }
+    }
+    else
+    {
+      value=provider.getStatValue(1,level);
+    }
+    return value;
   }
 }
