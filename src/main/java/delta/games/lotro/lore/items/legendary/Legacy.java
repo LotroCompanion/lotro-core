76176ab9bd341da5fa194a50f34c56e5d6685478
Damien Morcellet <dmorcellet@free.fr>
2019-01-20 18:45:29 +0100

    Implementation of the legacy description

--- src/main/java/delta/games/lotro/lore/items/legendary/Legacy.java
+++ src/main/java/delta/games/lotro/lore/items/legendary/Legacy.java
@@ -1,9 +1,128 @@
 package delta.games.lotro.lore.items.legendary;
 
+import delta.games.lotro.common.Identifiable;
+import delta.games.lotro.common.stats.StatsProvider;
+
 /**
+ * Legacy description.
  * @author DAM
  */
-public class Legacy
+public class Legacy implements Identifiable
 {
-  // TODO
+  private int _id;
+  private int _maxInitialLevel;
+  private int _maxTier;
+  private LegacyType _type;
+  //private CharacterClass _requiredClass;
+  //private boolean _imbued;
+  private StatsProvider _statsProvider;
+
+  /**
+   * Constructor.
+   */
+  public Legacy()
+  {
+    // Nothing!
+  }
+
+  /**
+   * Get the identifier of this legacy.
+   * @return an identifier.
+   */
+  public int getIdentifier()
+  {
+    return _id;
+  }
+
+  /**
+   * Set the identifier of this legacy.
+   * @param id the identifier to set.
+   */
+  public void setId(int id)
+  {
+    _id=id;
+  }
+
+  /**
+   * Get the maximum initial level.
+   * @return the maximum initial level.
+   */
+  public int getMaxInitialLevel()
+  {
+    return _maxInitialLevel;
+  }
+
+  /**
+   * Set the maximum initial level.
+   * @param maxInitialLevel the maximum initial level to set.
+   */
+  public void setMaxInitialLevel(int maxInitialLevel)
+  {
+    _maxInitialLevel=maxInitialLevel;
+  }
+
+  /**
+   * Get the maximum tier.
+   * @return the maximum tier.
+   */
+  public int getMaxTier()
+  {
+    return _maxTier;
+  }
+
+  /**
+   * Set the maximum tier.
+   * @param maxTier the maximum tier to set.
+   */
+  public void setMaxTier(int maxTier)
+  {
+    _maxTier=maxTier;
+  }
+
+  /**
+   * Get the legacy type.
+   * @return a legacy type.
+   */
+  public LegacyType getType()
+  {
+    return _type;
+  }
+
+  /**
+   * Set the legacy type.
+   * @param type the type to set.
+   */
+  public void setType(LegacyType type)
+  {
+    _type=type;
+  }
+
+  /**
+   * Get the stats provider.
+   * @return the stats provider.
+   */
+  public StatsProvider getStatsProvider()
+  {
+    return _statsProvider;
+  }
+
+  /**
+   * Set the stats provider.
+   * @param statsProvider Stats provider.
+   */
+  public void setStatsProvider(StatsProvider statsProvider)
+  {
+    _statsProvider=statsProvider;
+  }
+
+  @Override
+  public String toString()
+  {
+    StringBuilder sb=new StringBuilder();
+    sb.append("Legacy: ID=").append(_id);
+    sb.append(", type=").append(_type);
+    sb.append(", max tier=").append(_maxTier);
+    sb.append(", stats=").append(_statsProvider);
+    return sb.toString();
+  }
 }
---
+++ src/main/java/delta/games/lotro/lore/items/legendary/LegacyType.java
@@ -0,0 +1,33 @@
+package delta.games.lotro.lore.items.legendary;
+
+/**
+ * Legacy type.
+ * @author DAM
+ */
+public enum LegacyType
+{
+  /**
+   * Stat legacy.
+   */
+  STAT,
+  /**
+   * Class skill legacy.
+   */
+  CLASS,
+  /**
+   * DPS legacy.
+   */
+  DPS,
+  /**
+   * Outgoing healing legacy.
+   */
+  OUTGOING_HEALING,
+  /**
+   * Incoming healing legacy.
+   */
+  INCOMING_HEALING,
+  /**
+   * Tactical DPS legacy.
+   */
+  TACTICAL_DPS
+}
