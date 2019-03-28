e1c2f185896ac9350777cb1cb55a0301e4fe77bf
Damien Morcellet <dmorcellet@free.fr>
2019-03-28 22:39:19 +0100

    Updates after the removal of the quests index

--- src/main/java/delta/games/lotro/lore/quests/QuestsManager.java
+++ src/main/java/delta/games/lotro/lore/quests/QuestsManager.java
@@ -9,8 +9,6 @@ import delta.common.utils.cache.WeakReferencesCache;
 import delta.common.utils.files.archives.ArchiveManager;
 import delta.games.lotro.config.DataFiles;
 import delta.games.lotro.config.LotroCoreConfig;
-import delta.games.lotro.lore.quests.index.QuestsIndex;
-import delta.games.lotro.lore.quests.index.io.xml.QuestsIndexXMLParser;
 import delta.games.lotro.lore.quests.io.xml.QuestXMLParser;
 
 /**
@@ -22,11 +20,10 @@ public final class QuestsManager
   private static final Logger LOGGER=Logger.getLogger(QuestsManager.class);
 
   private static QuestsManager _instance=new QuestsManager();
-  
-  private QuestsIndex _index;
+
   private ArchiveManager _archive;
   private WeakReferencesCache<Integer,QuestDescription> _cache;
-  
+
   /**
    * Get the sole instance of this class.
    * @return the sole instance of this class.
@@ -46,16 +43,6 @@ public final class QuestsManager
     File questsArchive=new File(dir,"data/lore/quests.zip");
     _archive=new ArchiveManager(questsArchive);
     _archive.open();
-    loadIndex();
-  }
-
-  /**
-   * Get the quests index.
-   * @return the quests index.
-   */
-  public QuestsIndex getIndex()
-  {
-    return _index;
   }
 
   /**
@@ -100,20 +87,5 @@ public final class QuestsManager
       }
     }
     return ret;
-  }
-
-  private void loadIndex()
-  {
-    File dir=LotroCoreConfig.getInstance().getFile(DataFiles.ROOT);
-    File questIndexFile=new File(dir,"data/lore/indexes/questsIndex.xml");
-    if (questIndexFile.exists())
-    {
-      QuestsIndexXMLParser parser=new QuestsIndexXMLParser();
-      _index=parser.parseXML(questIndexFile);
-    }
-    else
-    {
-      _index=new QuestsIndex();
-    }
   }
 }
