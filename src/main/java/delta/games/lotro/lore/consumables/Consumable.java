33c5c9efb4c3603fdd6bcd2563d5c7cdc690568e
Damien Morcellet <dmorcellet@free.fr>
2019-04-10 22:53:37 +0200

    Added a class for consumables

---
+++ src/main/java/delta/games/lotro/lore/consumables/Consumable.java
@@ -0,0 +1,90 @@
+package delta.games.lotro.lore.consumables;
+
+import delta.games.lotro.common.Identifiable;
+import delta.games.lotro.common.stats.StatsProvider;
+
+/**
+ * Consumable.
+ * @author DAM
+ */
+public class Consumable implements Identifiable
+{
+  private int _identifier;
+  private String _name;
+  private String _icon;
+  private String _category;
+  private StatsProvider _provider;
+
+  /**
+   * Constructor.
+   * @param id Item identifier.
+   * @param name Item name.
+   * @param icon Item icon.
+   * @param category Item category.
+   */
+  public Consumable(int id, String name, String icon, String category)
+  {
+    _identifier=id;
+    _name=name;
+    _icon=icon;
+    _category=category;
+    _provider=new StatsProvider();
+  }
+
+  /**
+   * Get the identifier.
+   * @return an identifier.
+   */
+  public int getIdentifier()
+  {
+    return _identifier;
+  }
+
+  /**
+   * Get the consumable name.
+   * @return a name.
+   */
+  public String getName()
+  {
+    return _name;
+  }
+
+  /**
+   * Get the consumable icon.
+   * @return an icon name.
+   */
+  public String getIcon()
+  {
+    return _icon;
+  }
+
+  /**
+   * Get the consumable category.
+   * @return a category.
+   */
+  public String getCategory()
+  {
+    return _category;
+  }
+
+  /**
+   * Get the stats provider for this consumable.
+   * @return a stats provider.
+   */
+  public StatsProvider getProvider()
+  {
+    return _provider;
+  }
+
+  @Override
+  public String toString()
+  {
+    StringBuilder sb=new StringBuilder();
+    sb.append("Consumable: ID=").append(_identifier);
+    sb.append(", name=").append(_name);
+    sb.append(", category=").append(_category);
+    sb.append(", iconID=").append(_icon);
+    sb.append(", stats=").append(_provider);
+    return sb.toString();
+  }
+}
