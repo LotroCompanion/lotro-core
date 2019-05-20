d8f3fb311b1ec120367d06f3afaf94d7d44e54af
Damien Morcellet <dmorcellet@free.fr>
2019-05-20 20:37:11 +0200

    Added all condition types in the ConditionType enum.

--- src/main/java/delta/games/lotro/lore/quests/objectives/ConditionType.java
+++ src/main/java/delta/games/lotro/lore/quests/objectives/ConditionType.java
@@ -7,11 +7,147 @@ package delta.games.lotro.lore.quests.objectives;
 public enum ConditionType
 {
   /**
+   * Enter detection.
+   */
+  ENTER_DETECTION,
+  /**
+   * Leave detection.
+   */
+  LEAVE_DETECTION,
+  /**
+   * NPC used.
+   */
+  NPC_USED,
+  /**
+   * Skill applied.
+   */
+  SKILL_APPLIED,
+  /**
+   * Item used.
+   */
+  ITEM_USED,
+  /**
+   * Detecting.
+   */
+  DETECTING,
+  /**
+   * External inventory item.
+   */
+  EXTERNAL_INVENTORY_ITEM,
+  /**
+   * NPC talk.
+   */
+  NPC_TALK,
+  /**
+   * Channeling.
+   */
+  CHANNELING,
+  /**
+   * Time expired.
+   */
+  TIME_EXPIRED,
+  /**
+   * Item talk.
+   */
+  ITEM_TALK,
+  /**
+   * Level.
+   */
+  LEVEL,
+  /**
+   * Clear camp.
+   */
+  CLEAR_CAMP,
+  /**
+   * Landmark detection.
+   */
+  LANDMARK_DETECTION,
+  /**
    * Monster died.
    */
   MONSTER_DIED,
   /**
+   * Emote.
+   */
+  EMOTE,
+  /**
+   * Player died.
+   */
+  PLAYER_DIED,
+  /**
+   * Skill used.
+   */
+  SKILL_USED,
+  /**
+   * Kung fu.
+   */
+  KUNG_FU,
+  /**
+   * Escort.
+   */
+  ESCORT,
+  /**
+   * Self died.
+   */
+  SELF_DIED,
+  /**
+   * Inventory item.
+   */
+  INVENTORY_ITEM,
+  /**
    * Quest/deed completed.
    */
-  QUEST_COMPLETE
+  QUEST_COMPLETE,
+  /**
+   * Craft recipe execution.
+   */
+  CRAFT_RECIPE_EXECUTION,
+  /**
+   * World event condition.
+   */
+  WORLD_EVENT_CONDITION,
+  /**
+   * Session finished.
+   */
+  SESSION_FINISHED,
+  /**
+   * Resource set.
+   */
+  RESOURCE_SET,
+  /**
+   * Hobby item.
+   */
+  HOBBY_ITEM,
+  /**
+   * Item advancement.
+   */
+  ITEM_ADVANCEMENT,
+  /**
+   * Dismounted.
+   */
+  DISMOUNTED,
+  /**
+   * Faction level.
+   */
+  FACTION_LEVEL,
+  /**
+   * Teleported.
+   */
+  TELEPORTED,
+  /**
+   * Enter player AOI.
+   */
+  ENTER_PLAYER_AOI,
+  /**
+   * Corpse used.
+   */
+  CORPSE_USED,
+  /**
+   * Script callback.
+   */
+  SCRIPT_CALLBACK,
+  /**
+   * Quest bestowed.
+   */
+  QUEST_BESTOWED
 }
