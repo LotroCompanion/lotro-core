6ebc7c90f544c92fc10cf248084f6308dae08c11
Damien Morcellet <dmorcellet@free.fr>
2019-08-02 08:29:23 +0200

    Added a class with constants related to legendary stuff

---
+++ src/main/java/delta/games/lotro/lore/items/legendary/LegendaryConstants.java
@@ -0,0 +1,17 @@
+package delta.games.lotro.lore.items.legendary;
+
+/**
+ * Constants related to the legendary stuff.
+ * @author DAM
+ */
+public class LegendaryConstants
+{
+  /**
+   * Maximum number of legacies.
+   */
+  public static final int MAX_LEGACIES=7;
+  /**
+   * Maximum level for a legendary weapon.
+   */
+  public static final int MAX_LEVEL=70;
+}
