1efe581fbc668dea9d85ccd93a42d62d799ca010
Damien Morcellet <dmorcellet@free.fr>
2020-02-23 20:00:25 +0100

    Added relic references from quests/deeds rewards

--- src/main/java/delta/games/lotro/lore/xrefs/relics/RelicReferencesBuilder.java
+++ src/main/java/delta/games/lotro/lore/xrefs/relics/RelicReferencesBuilder.java
@@ -4,11 +4,20 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import delta.games.lotro.common.rewards.RelicReward;
+import delta.games.lotro.common.rewards.RewardElement;
+import delta.games.lotro.common.rewards.Rewards;
+import delta.games.lotro.common.rewards.SelectableRewardElement;
 import delta.games.lotro.config.DataFiles;
 import delta.games.lotro.config.LotroCoreConfig;
+import delta.games.lotro.lore.deeds.DeedDescription;
+import delta.games.lotro.lore.deeds.DeedsManager;
 import delta.games.lotro.lore.items.Container;
 import delta.games.lotro.lore.items.io.xml.ContainerXMLParser;
 import delta.games.lotro.lore.items.legendary.relics.RelicsContainer;
+import delta.games.lotro.lore.quests.Achievable;
+import delta.games.lotro.lore.quests.QuestDescription;
+import delta.games.lotro.lore.quests.QuestsManager;
 
 /**
  * Finds references to relics.
@@ -27,20 +36,22 @@ public class RelicReferencesBuilder
   }
 
   /**
-   * Search for an item.
-   * @param itemId Item identifier.
+   * Search for a relic.
+   * @param relicId Relic identifier.
    * @return the found references.
    */
-  public List<RelicReference<?>> inspectItem(int itemId)
+  public List<RelicReference<?>> inspectItem(int relicId)
   {
     _storage.clear();
-    findInContainers(itemId);
+    findInQuestRewards(relicId);
+    findInDeedRewards(relicId);
+    findInContainers(relicId);
     List<RelicReference<?>> ret=new ArrayList<RelicReference<?>>(_storage);
     _storage.clear();
     return ret;
   }
 
-  private void findInContainers(int itemId)
+  private void findInContainers(int relicId)
   {
     File containersFile=LotroCoreConfig.getInstance().getFile(DataFiles.CONTAINERS);
     List<Container> containers=new ContainerXMLParser().parseXML(containersFile);
@@ -49,11 +60,69 @@ public class RelicReferencesBuilder
       if (container instanceof RelicsContainer)
       {
         RelicsContainer relicsContainer=(RelicsContainer)container;
-        boolean found=relicsContainer.contains(itemId);
+        boolean found=relicsContainer.contains(relicId);
         if (found)
         {
           _storage.add(new RelicReference<Container>(container,RelicRole.CONTAINED_IN));
         }
+      }
+    }
+  }
+
+  private void findInQuestRewards(int relicId)
+  {
+    QuestsManager questsManager=QuestsManager.getInstance();
+    List<QuestDescription> quests=questsManager.getAll();
+    for(QuestDescription quest : quests)
+    {
+      findInQuest(quest,relicId);
+    }
+  }
+
+  private void findInQuest(QuestDescription quest, int relicId)
+  {
+    findInRewards(quest,quest.getRewards(),relicId);
+  }
+
+  private void findInDeedRewards(int relicId)
+  {
+    DeedsManager deedsManager=DeedsManager.getInstance();
+    List<DeedDescription> deeds=deedsManager.getAll();
+    for(DeedDescription deed : deeds)
+    {
+      findInDeed(deed,relicId);
+    }
+  }
+
+  private void findInDeed(DeedDescription deed, int itemId)
+  {
+    findInRewards(deed,deed.getRewards(),itemId);
+  }
+
+  private void findInRewards(Achievable context, Rewards rewards, int relicId)
+  {
+    List<RewardElement> elements=rewards.getRewardElements();
+    findInRewardsElements(context,elements,relicId);
+  }
+
+  private void findInRewardsElements(Achievable context, List<RewardElement> elements, int relicId)
+  {
+    for(RewardElement element : elements)
+    {
+      if (element instanceof RelicReward)
+      {
+        RelicReward itemReward=(RelicReward)element;
+        int relicRewardId=itemReward.getRelicProxy().getId();
+        if (relicRewardId==relicId)
+        {
+          RelicRole role=(context instanceof QuestDescription)?RelicRole.QUEST_REWARD:RelicRole.DEED_REWARD;
+          _storage.add(new RelicReference<Achievable>(context,role));
+        }
+      }
+      else if (element instanceof SelectableRewardElement)
+      {
+        SelectableRewardElement selectableReward=(SelectableRewardElement)element;
+        findInRewardsElements(context,selectableReward.getElements(),relicId);
       }
     }
   }
--- src/main/java/delta/games/lotro/lore/xrefs/relics/RelicRole.java
+++ src/main/java/delta/games/lotro/lore/xrefs/relics/RelicRole.java
@@ -7,6 +7,14 @@ package delta.games.lotro.lore.xrefs.relics;
 public enum RelicRole
 {
   /**
+   * Relic is a reward of quest.
+   */
+  QUEST_REWARD,
+  /**
+   * Relic is a reward of deed.
+   */
+  DEED_REWARD,
+  /**
    * Relic may come from container.
    */
   CONTAINED_IN
