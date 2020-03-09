f8e358afd73b0ea8148ff148012dc8279707fef4
Damien Morcellet <dmorcellet@free.fr>
2020-03-09 11:17:01 +0100

    Fixed NPE in FactionLevelComparator

--- src/main/java/delta/games/lotro/lore/reputation/FactionLevelComparator.java
+++ src/main/java/delta/games/lotro/lore/reputation/FactionLevelComparator.java
@@ -10,12 +10,12 @@ public class FactionLevelComparator implements Comparator<FactionLevel>
 {
   public int compare(FactionLevel factionLevel1, FactionLevel factionLevel2)
   {
-    int tier1=factionLevel1.getTier();
-    int tier2=factionLevel2.getTier();
+    int tier1=factionLevel1!=null?factionLevel1.getTier():-100;
+    int tier2=factionLevel2!=null?factionLevel2.getTier():-100;
     if (tier1>tier2) return 1;
     if (tier1<tier2) return -1;
-    String name1=factionLevel1.getName();
-    String name2=factionLevel2.getName();
+    String name1=factionLevel1!=null?factionLevel1.getName():"";
+    String name2=factionLevel2!=null?factionLevel2.getName():"";
     return name1.compareTo(name2);
   }
 }
