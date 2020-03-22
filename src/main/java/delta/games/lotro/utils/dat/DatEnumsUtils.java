48e4607776f13a935df782b1c9e40766031f1351
Damien Morcellet <dmorcellet@free.fr>
2020-03-22 18:41:11 +0100

    Added methods in DatEnumsUtils

--- src/main/java/delta/games/lotro/utils/dat/DatEnumsUtils.java
+++ src/main/java/delta/games/lotro/utils/dat/DatEnumsUtils.java
@@ -1,5 +1,6 @@
 package delta.games.lotro.utils.dat;
 
+import delta.games.lotro.character.CharacterEquipment.EQUIMENT_SLOT;
 import delta.games.lotro.common.CharacterClass;
 import delta.games.lotro.common.Race;
 
@@ -97,6 +98,60 @@ public class DatEnumsUtils
 
     // Stout-axe dwarf
     if (nationalityId==27) return "Mordor Mountains";
+    return null;
+  }
+
+  /**
+   * Indicates if the given slot code is for an equipped item.
+   * @param slotCode Code to use.
+   * @return <code>true</code> if equipped, <code>false</code> otherwise.
+   */
+  public static boolean isEquipped(int slotCode)
+  {
+    return ((slotCode&1L<<29)!=0);
+  }
+
+  /**
+   * Indicates if the given slot code means an item in the bags.
+   * @param slotCode Code to use.
+   * @return <code>true</code> if it does, <code>false</code> otherwise.
+   */
+  public static boolean isInBags(int slotCode)
+  {
+    return ((slotCode&1L<<30)!=0);
+  }
+
+  /**
+   * Get an equipment slot from a DAT enum code.
+   * @param slotCode Input code.
+   * @return An equipment slot or <code>null</code> if not supported.
+   */
+  public static EQUIMENT_SLOT getEquipmentSlot(int slotCode)
+  {
+    if ((slotCode&1L<<1)!=0) return EQUIMENT_SLOT.HEAD;
+    if ((slotCode&1L<<2)!=0) return EQUIMENT_SLOT.BREAST;
+    if ((slotCode&1L<<3)!=0) return EQUIMENT_SLOT.LEGS;
+    if ((slotCode&1L<<4)!=0) return EQUIMENT_SLOT.HANDS;
+    if ((slotCode&1L<<5)!=0) return EQUIMENT_SLOT.FEET;
+    if ((slotCode&1L<<6)!=0) return EQUIMENT_SLOT.SHOULDER;
+    if ((slotCode&1L<<7)!=0) return EQUIMENT_SLOT.BACK;
+    if ((slotCode&1L<<8)!=0) return EQUIMENT_SLOT.LEFT_WRIST;
+    if ((slotCode&1L<<9)!=0) return EQUIMENT_SLOT.RIGHT_WRIST;
+    if ((slotCode&1L<<10)!=0) return EQUIMENT_SLOT.NECK;
+    if ((slotCode&1L<<11)!=0) return EQUIMENT_SLOT.LEFT_FINGER;
+    if ((slotCode&1L<<12)!=0) return EQUIMENT_SLOT.RIGHT_FINGER;
+    if ((slotCode&1L<<13)!=0) return EQUIMENT_SLOT.LEFT_EAR;
+    if ((slotCode&1L<<14)!=0) return EQUIMENT_SLOT.RIGHT_EAR;
+    if ((slotCode&1L<<15)!=0) return EQUIMENT_SLOT.POCKET;
+    if ((slotCode&1L<<16)!=0) return EQUIMENT_SLOT.MAIN_MELEE;
+    if ((slotCode&1L<<17)!=0) return EQUIMENT_SLOT.OTHER_MELEE;
+    if ((slotCode&1L<<18)!=0) return EQUIMENT_SLOT.RANGED;
+    if ((slotCode&1L<<19)!=0) return EQUIMENT_SLOT.TOOL;
+    if ((slotCode&1L<<20)!=0) return EQUIMENT_SLOT.CLASS_ITEM;
+    //if ((slotCode&1L<<21)!=0) return EQUIMENT_SLOT.BRIDLE;
+    if ((slotCode&1L<<22)!=0) return EQUIMENT_SLOT.MAIN_HAND_AURA;
+    if ((slotCode&1L<<23)!=0) return EQUIMENT_SLOT.OFF_HAND_AURA;
+    if ((slotCode&1L<<24)!=0) return EQUIMENT_SLOT.RANGED_AURA;
     return null;
   }
 }
