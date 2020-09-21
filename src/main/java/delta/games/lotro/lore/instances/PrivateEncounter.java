d33183a499af1194049db19c38e707b35729ba83
Damien Morcellet <dmorcellet@free.fr>
2020-09-21 22:19:24 +0200

    Initial version of the PrivateEncounter POJOs

--- src/main/java/delta/games/lotro/lore/instances/PrivateEncounter.java
+++ src/main/java/delta/games/lotro/lore/instances/PrivateEncounter.java
@@ -1,7 +1,9 @@
 package delta.games.lotro.lore.instances;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import delta.common.utils.text.EndOfLine;
 import delta.games.lotro.common.Identifiable;
 import delta.games.lotro.lore.geo.BlockReference;
 
@@ -17,6 +19,9 @@ public class PrivateEncounter implements Identifiable
   private int _contentLayerId;
   private List<BlockReference> _blocks;
   private Integer _questId;
+  // Quests to bestow (array)
+  private List<Integer> _questsToBestow;
+  // TODO LATER
   // Start position
   // Success position
   // Requirements
@@ -26,10 +31,169 @@ public class PrivateEncounter implements Identifiable
   // - quests
   // - world conditions
   // Maximum number of players
-  private int _maxPlayers;
+  private Integer _maxPlayers;
 
+  /**
+   * Constructor.
+   * @param id Identifier.
+   */
+  public PrivateEncounter(int id)
+  {
+    _id=id;
+    _blocks=new ArrayList<BlockReference>();
+    _questsToBestow=new ArrayList<Integer>();
+  }
+
+  @Override
   public int getIdentifier()
   {
     return _id;
+  }
+
+  /**
+   * Get the instance name.
+   * @return the instance name.
+   */
+  public String getName()
+  {
+    return _name;
+  }
+
+  /**
+   * Set the instance name.
+   * @param name Name to set.
+   */
+  public void setName(String name)
+  {
+    _name=name;
+  }
+
+  /**
+   * Get the instance description.
+   * @return the instance description.
+   */
+  public String getDescription()
+  {
+    return _description;
+  }
+
+  /**
+   * Set the instance description.
+   * @param description Description to set.
+   */
+  public void setDescription(String description)
+  {
+    _description=description;
+  }
+
+  /**
+   * Get the identifier of the associated content layer.
+   * @return a content layer identifier.
+   */
+  public int getContentLayerId()
+  {
+    return _contentLayerId;
+  }
+
+  /**
+   * Set the content layer identifier.
+   * @param contentLayerId Identifier to set.
+   */
+  public void setContentLayerId(int contentLayerId)
+  {
+    _contentLayerId=contentLayerId;
+  }
+
+  /**
+   * Add a block reference.
+   * @param block Bloc reference to add.
+   */
+  public void addBlock(BlockReference block)
+  {
+    _blocks.add(block);
+  }
+
+  /**
+   * Get the blocks for this instance.
+   * @return a list of block references.
+   */
+  public List<BlockReference> getBlocks()
+  {
+    return _blocks;
+  }
+
+  /**
+   * Get the identifier of the associated quest.
+   * @return A quest identifier or <code>null</code>.
+   */
+  public Integer getQuestId()
+  {
+    return _questId;
+  }
+
+  /**
+   * Set the identifier of the associated quest.
+   * @param questId Quest identifier (may be <code>null</code>).
+   */
+  public void setQuestId(Integer questId)
+  {
+    _questId=questId;
+  }
+
+  /**
+   * Add an identifier for a 'quest to bestow'.
+   * @param questId Quest identifier.
+   */
+  public void addQuestToBestow(int questId)
+  {
+    _questsToBestow.add(Integer.valueOf(questId));
+  }
+
+  /**
+   * Get the maximum number of players in the instance, if any.
+   * @return A player count, or <code>null</code>.
+   */
+  public Integer getMaxPlayers()
+  {
+    return _maxPlayers;
+  }
+
+  /**
+   * Set the maximum number of players in this instance.
+   * @param maxPlayers Player count to set, may be <code>null</code>.
+   */
+  public void setMaxPlayers(Integer maxPlayers)
+  {
+    _maxPlayers=maxPlayers;
+  }
+
+  protected void dump(StringBuilder sb)
+  {
+    String className=getClass().getSimpleName();
+    sb.append(className).append(": ID=").append(_id);
+    sb.append(", name=").append(_name);
+    sb.append(", content layer=").append(_contentLayerId).append(EndOfLine.NATIVE_EOL);
+    sb.append("Description=").append(_description).append(EndOfLine.NATIVE_EOL);
+    sb.append("Blocks=").append(_blocks).append(EndOfLine.NATIVE_EOL);
+    if (_questId!=null)
+    {
+      sb.append("Quest=").append(_questId).append(EndOfLine.NATIVE_EOL);
+    }
+    if (_questsToBestow.size()>0)
+    {
+      sb.append("Quest(s) to bestow=").append(_questsToBestow).append(EndOfLine.NATIVE_EOL);
+    }
+    if (_maxPlayers!=null)
+    {
+      sb.append("Max players=").append(_maxPlayers).append(EndOfLine.NATIVE_EOL);
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    StringBuilder sb=new StringBuilder();
+    dump(sb);
+    return sb.toString().trim();
   }
 }
--- src/main/java/delta/games/lotro/lore/instances/SkirmishPrivateEncounter.java
+++ src/main/java/delta/games/lotro/lore/instances/SkirmishPrivateEncounter.java
@@ -1,6 +1,7 @@
 package delta.games.lotro.lore.instances;
 
-import java.util.List;
+import delta.common.utils.text.EndOfLine;
+
 
 /**
  * Private encounter of type 'Skirmish/Big battle/Classic instance'.
@@ -9,13 +10,47 @@ import java.util.List;
 public class SkirmishPrivateEncounter extends PrivateEncounter
 {
   // Rewards configuration
-  // Quests to bestow (array)
-  private List<Integer> _questsToBestow;
   // Difficulty tiers
   // Group sizes
   // Min/max level scale
   // Category (Defensive...) // WorldJoin_EncounterCategory
   // Type (Skirmish...) // WorldJoin_EncounterType
   // Skirmish_RestrictionDescription: #1: You must have completed the 'Helm's Dike' battle.
-  private Integer _levelScaling; // Skirmish_Template_LevelScalingLevel: 100
+  private Integer _levelScaling;
+
+  /**
+   * Constructor.
+   * @param id Identifier.
+   */
+  public SkirmishPrivateEncounter(int id)
+  {
+    super(id);
+  }
+
+  /**
+   * Get the character level scaling for this instance.
+   * @return A level or <code>null</code>.
+   */
+  public Integer getLevelScaling()
+  {
+    return _levelScaling;
+  }
+
+  /**
+   * Set the character level scaling.
+   * @param levelScaling Level to set (may be <code>null</code>).
+   */
+  public void setLevelScaling(Integer levelScaling)
+  {
+    _levelScaling=levelScaling;
+  }
+
+  protected void dump(StringBuilder sb)
+  {
+    super.dump(sb);
+    if (_levelScaling!=null)
+    {
+      sb.append("Level scaling=").append(_levelScaling).append(EndOfLine.NATIVE_EOL);
+    }
+  }
 }
