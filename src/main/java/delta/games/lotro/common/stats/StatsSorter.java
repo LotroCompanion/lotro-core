6910778ab4878865b320ce1b2d061ddc175b510e
Damien Morcellet <dmorcellet@free.fr>
2020-11-05 22:05:32 +0100

    Dead code removal

--- src/main/java/delta/games/lotro/common/stats/StatsSorter.java
+++ src/main/java/delta/games/lotro/common/stats/StatsSorter.java
@@ -6,7 +6,6 @@ import java.util.Comparator;
 import java.util.List;
 
 import delta.common.utils.collections.CompoundComparator;
-import delta.games.lotro.common.IdentifiableComparator;
 
 /**
  * Stats sorting utilities.
@@ -14,19 +13,6 @@ import delta.games.lotro.common.IdentifiableComparator;
  */
 public class StatsSorter
 {
-  /**
-   * Sort stats for internal use.
-   * @param stats Stats to sort.
-   */
-  public static void sortStatsForInternal(List<StatDescription> stats)
-  {
-    List<Comparator<StatDescription>> comparators=new ArrayList<Comparator<StatDescription>>();
-    comparators.add(new StatDescriptionIndexComparator());
-    comparators.add(new IdentifiableComparator<StatDescription>());
-    CompoundComparator<StatDescription> c=new CompoundComparator<StatDescription>(comparators);
-    Collections.sort(stats,c);
-  }
-
   /**
    * Sort stats for UI use.
    * @param stats Stats to sort.
