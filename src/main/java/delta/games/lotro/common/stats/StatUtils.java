6116e807c4990917ab4bad474d1a9f035e04e148
Damien Morcellet <dmorcellet@free.fr>
2020-11-20 18:49:36 +0100

    Moved stats display methods from lotro-companion to lotro-core

--- src/main/java/delta/games/lotro/common/stats/StatUtils.java
+++ src/main/java/delta/games/lotro/common/stats/StatUtils.java
@@ -1,5 +1,11 @@
 package delta.games.lotro.common.stats;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import delta.games.lotro.character.stats.BasicStatsSet;
+import delta.games.lotro.utils.FixedDecimalsInteger;
 
 /**
  * Utility methods for stats.
@@ -7,6 +13,68 @@ package delta.games.lotro.common.stats;
  */
 public class StatUtils
 {
+  /**
+   * Get a displayable string for a stat value.
+   * @param value Value to display.
+   * @param percentage Indicates if the stat is a percentage or not.
+   * @return A displayable string.
+   */
+  public static String getStatDisplay(FixedDecimalsInteger value, boolean percentage)
+  {
+    String valueStr;
+    if (value!=null)
+    {
+      if (percentage)
+      {
+        valueStr=new DecimalFormat("#.#%").format(value.doubleValue()/100);
+      }
+      else
+      {
+        valueStr=String.format("%d",Integer.valueOf(value.intValue()));
+      }
+    }
+    else
+    {
+      valueStr="-";
+    }
+    return valueStr;
+  }
+
+  /**
+   * Get the display lines for some stats.
+   * @param stats Stats to display.
+   * @return A possibly empty but not <code>null</code> array of stat lines.
+   */
+  public static String[] getStatsDisplayLines(BasicStatsSet stats)
+  {
+    List<String> lines=new ArrayList<String>();
+    for(StatDescription stat : stats.getSortedStats())
+    {
+      if (stat.isVisible())
+      {
+        String line=getStatDisplay(stat,stats);
+        lines.add(line);
+      }
+    }
+    String[] ret=lines.toArray(new String[lines.size()]);
+    return ret;
+  }
+
+  /**
+   * Get the display line for a single stat.
+   * @param stat Stat to display.
+   * @param stats Stats to get value from.
+   * @return A displayable line.
+   */
+  public static String getStatDisplay(StatDescription stat, BasicStatsSet stats)
+  {
+    String statName=stat.getName();
+    FixedDecimalsInteger value=stats.getStat(stat);
+    String valueStr=getStatDisplay(value,stat.isPercentage());
+    String line=valueStr+" "+statName;
+    return line;
+  }
+
   /**
    * Fix the value of a stat:
    * <ul>
