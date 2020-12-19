bba33d740b2d2fe0ff060dc8f9bac0ffb4665c5f
Damien Morcellet <dmorcellet@free.fr>
2020-12-19 18:44:20 +0100

    Added constants used in quest/deed objectives

---
+++ src/main/java/delta/games/lotro/lore/quests/objectives/ObjectivesConstants.java
@@ -0,0 +1,18 @@
+package delta.games.lotro.lore.quests.objectives;
+
+/**
+ * Constants used in deed/quest objectives.
+ * @author DAM
+ */
+public class ObjectivesConstants
+{
+  /**
+   * Marker for a count value.
+   */
+  public static final String COUNT="{***}";
+
+  /**
+   * Marker for a count status.
+   */
+  public static final String COUNT_PATTERN=COUNT+"/"+COUNT;
+}
