c61226aed54d37d544d789d0f304e4a93dfbc30c
Damien Morcellet <dmorcellet@free.fr>
2021-06-01 13:24:10 +0200

    Moved here the trait points non-UI code (from lotro-companion)

---
+++ src/main/java/delta/games/lotro/character/traitPoints/comparators/TraitPointIdComparator.java
@@ -0,0 +1,28 @@
+package delta.games.lotro.character.traitPoints.comparators;
+
+import java.util.Comparator;
+
+import delta.games.lotro.character.traitPoints.TraitPoint;
+
+/**
+ * Comparator for trait points, using their label.
+ * @author DAM
+ */
+public class TraitPointIdComparator implements Comparator<TraitPoint>
+{
+  @Override
+  public int compare(TraitPoint tp1, TraitPoint tp2)
+  {
+    String label1=tp1.getId();
+    String label2=tp2.getId();
+    if (label1!=null)
+    {
+      if (label2!=null)
+      {
+        return label1.compareTo(label2);
+      }
+      return 1;
+    }
+    return (label2!=null)?-1:0;
+  }
+}
