1f4b58201323402dee911413683f239a7c8066d6
Damien Morcellet <dmorcellet@free.fr>
2021-08-02 15:20:44 +0200

    Added new classes for tasks stats

---
+++ src/main/java/delta/games/lotro/character/tasks/TaskStatus.java
@@ -0,0 +1,43 @@
+package delta.games.lotro.character.tasks;
+
+import delta.games.lotro.character.achievables.AchievableStatus;
+import delta.games.lotro.lore.tasks.Task;
+
+/**
+ * Status of a single task quest.
+ * @author DAM
+ */
+public class TaskStatus
+{
+  private Task _task;
+  private AchievableStatus _status;
+
+  /**
+   * Constructor.
+   * @param task Associated task.
+   * @param status Associated quest status.
+   */
+  public TaskStatus(Task task, AchievableStatus status)
+  {
+    _task=task;
+    _status=status;
+  }
+
+  /**
+   * Get the associated task.
+   * @return a task.
+   */
+  public Task getTask()
+  {
+    return _task;
+  }
+
+  /**
+   * Get the associated quest status.
+   * @return a status.
+   */
+  public AchievableStatus getStatus()
+  {
+    return _status;
+  }
+}
