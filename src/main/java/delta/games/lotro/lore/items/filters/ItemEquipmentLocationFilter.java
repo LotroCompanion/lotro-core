092f4b2171bb1607c243044094d0a40df22928c0
Damien Morcellet <dmorcellet@free.fr>
2021-09-06 16:25:40 +0200

    Get persistence facilities for the ItemEquipmentLocationFilter

--- src/main/java/delta/games/lotro/lore/items/filters/ItemEquipmentLocationFilter.java
+++ src/main/java/delta/games/lotro/lore/items/filters/ItemEquipmentLocationFilter.java
@@ -1,6 +1,9 @@
 package delta.games.lotro.lore.items.filters;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import delta.games.lotro.lore.items.EquipmentLocation;
@@ -67,5 +70,48 @@ public class ItemEquipmentLocationFilter implements ItemFilter
   public boolean accept(Item item)
   {
     return _locations.contains(item.getEquipmentLocation());
+  }
+
+  /**
+   * Get the state of this filter as a list of strings.
+   * @return A list of strings.
+   */
+  public List<String> asString()
+  {
+    List<String> ret=new ArrayList<String>();
+    for(EquipmentLocation location : _locations)
+    {
+      if (location!=null)
+      {
+        ret.add(location.getKey());
+      }
+      else
+      {
+        ret.add("null");
+      }
+    }
+    Collections.sort(ret);
+    return ret;
+  }
+
+  /**
+   * Set the state of this filter from the given list of string.
+   * @param input Input list.
+   */
+  public void loadFromString(List<String> input)
+  {
+    Set<EquipmentLocation> locations=new HashSet<EquipmentLocation>();
+    for(String locationKey : input)
+    {
+      if ("null".equals(locationKey))
+      {
+        locations.add(null);
+      }
+      else
+      {
+        locations.add(EquipmentLocation.getByKey(locationKey));
+      }
+    }
+    setLocations(locations);
   }
 }
