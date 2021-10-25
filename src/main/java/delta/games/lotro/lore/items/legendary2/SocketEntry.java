0734963c8cabbb8f616912242c90985cd6cf55ce
Damien Morcellet <dmorcellet@free.fr>
2021-10-25 16:18:40 +0200

    Added facilities to tell if a tracery socket is enabled or not

--- src/main/java/delta/games/lotro/lore/items/legendary2/SocketEntry.java
+++ src/main/java/delta/games/lotro/lore/items/legendary2/SocketEntry.java
@@ -1,6 +1,8 @@
 package delta.games.lotro.lore.items.legendary2;
 
 import delta.games.lotro.common.enums.SocketType;
+import delta.games.lotro.lore.items.Item;
+import delta.games.lotro.lore.items.ItemInstance;
 
 /**
  * Definition of a socket entry.
@@ -38,6 +40,28 @@ public class SocketEntry
   public int getUnlockItemLevel()
   {
     return _unlockItemLevel;
+  }
+
+  /**
+   * Indicates if this socket is enabled or not for the given LI instance.
+   * @param itemInstance Item instance to use.
+   * @return <code>true</code> if it is, <code>false</code> otherwise.
+   */
+  public boolean isEnabled(ItemInstance<? extends Item> itemInstance)
+  {
+    Integer itemLevel=itemInstance.getEffectiveItemLevel();
+    int itemLevelInt=(itemLevel!=null)?itemLevel.intValue():1;
+    return isEnabled(itemLevelInt);
+  }
+
+  /**
+   * Indicates if this socket is enabled or not for a LI with the given item level.
+   * @param itemLevel Item level to use.
+   * @return <code>true</code> if it is, <code>false</code> otherwise.
+   */
+  public boolean isEnabled(int itemLevel)
+  {
+    return itemLevel>=_unlockItemLevel;
   }
 
   @Override
