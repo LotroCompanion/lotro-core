07807a07f5d2ff16f64ac43f60776f6d283c5733
Damien Morcellet <dmorcellet@free.fr>
2021-12-03 18:31:21 +0100

    Added enum ItemClass

---
+++ src/main/java/delta/games/lotro/common/enums/ItemClass.java
@@ -0,0 +1,14 @@
+package delta.games.lotro.common.enums;
+
+/**
+ * Item class.
+ * @author DAM
+ */
+public class ItemClass extends LotroEnumEntry
+{
+  @Override
+  public String toString()
+  {
+    return getLabel();
+  }
+}
