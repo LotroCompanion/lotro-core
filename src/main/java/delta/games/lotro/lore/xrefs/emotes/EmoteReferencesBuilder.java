291ff624441e9c2c6f887a98779e187f42a05663
Damien Morcellet <dmorcellet@free.fr>
2022-07-08 14:06:28 +0200

    Added x-ref support for emotes

---
+++ src/main/java/delta/games/lotro/lore/xrefs/emotes/EmoteReferencesBuilder.java
@@ -0,0 +1,91 @@
+package delta.games.lotro.lore.xrefs.emotes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import delta.games.lotro.common.rewards.EmoteReward;
+import delta.games.lotro.common.rewards.RewardElement;
+import delta.games.lotro.common.rewards.Rewards;
+import delta.games.lotro.common.rewards.SelectableRewardElement;
+import delta.games.lotro.lore.deeds.DeedDescription;
+import delta.games.lotro.lore.deeds.DeedsManager;
+import delta.games.lotro.lore.quests.Achievable;
+import delta.games.lotro.lore.quests.QuestDescription;
+import delta.games.lotro.lore.quests.QuestsManager;
+
+/**
+ * Finds references to emotes.
+ * @author DAM
+ */
+public class EmoteReferencesBuilder
+{
+  private List<EmoteReference<?>> _storage;
+
+  /**
+   * Constructor.
+   */
+  public EmoteReferencesBuilder()
+  {
+    _storage=new ArrayList<EmoteReference<?>>();
+  }
+
+  /**
+   * Search for an emote.
+   * @param emoteID Emote identifier.
+   * @return the found references.
+   */
+  public List<EmoteReference<?>> inspectEmote(int emoteID)
+  {
+    _storage.clear();
+    findInRewards(emoteID);
+    List<EmoteReference<?>> ret=new ArrayList<EmoteReference<?>>(_storage);
+    _storage.clear();
+    return ret;
+  }
+
+  private void findInRewards(int emoteID)
+  {
+    // Quests
+    QuestsManager questsManager=QuestsManager.getInstance();
+    List<QuestDescription> quests=questsManager.getAll();
+    for(QuestDescription quest : quests)
+    {
+      findInRewards(quest,quest.getRewards(),emoteID);
+    }
+    // Deeds
+    DeedsManager deedsManager=DeedsManager.getInstance();
+    List<DeedDescription> deeds=deedsManager.getAll();
+    for(DeedDescription deed : deeds)
+    {
+      findInRewards(deed,deed.getRewards(),emoteID);
+    }
+  }
+
+  private void findInRewards(Achievable context, Rewards rewards, int emoteID)
+  {
+    List<RewardElement> elements=rewards.getRewardElements();
+    findInRewardsElements(context,elements,emoteID);
+  }
+
+  private void findInRewardsElements(Achievable context, List<RewardElement> elements, int emoteID)
+  {
+    for(RewardElement element : elements)
+    {
+      if (element instanceof EmoteReward)
+      {
+        EmoteReward emoteReward=(EmoteReward)element;
+        int emoteRewardID=emoteReward.getEmoteProxy().getId();
+        if (emoteRewardID==emoteID)
+        {
+          EmoteRole role=EmoteRole.REGULAR_REWARD;
+          _storage.add(new EmoteReference<Achievable>(context,role));
+        }
+      }
+      else if (element instanceof SelectableRewardElement)
+      {
+        SelectableRewardElement selectableReward=(SelectableRewardElement)element;
+        findInRewardsElements(context,selectableReward.getElements(),emoteID);
+      }
+    }
+  }
+}
---
+++ src/main/java/delta/games/lotro/lore/xrefs/emotes/EmoteRole.java
@@ -0,0 +1,13 @@
+package delta.games.lotro.lore.xrefs.emotes;
+
+/**
+ * Emote roles.
+ * @author DAM
+ */
+public enum EmoteRole
+{
+  /**
+   * Emote provided as a regular quest/deed reward.
+   */
+  REGULAR_REWARD
+}
