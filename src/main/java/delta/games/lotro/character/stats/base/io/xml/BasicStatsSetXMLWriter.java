7547059587077580a56d41cf3e733f7dd7c01976
Damien Morcellet <dmorcellet@free.fr>
2022-07-17 22:07:52 +0200

    Secured the stats writer

--- src/main/java/delta/games/lotro/character/stats/base/io/xml/BasicStatsSetXMLWriter.java
+++ src/main/java/delta/games/lotro/character/stats/base/io/xml/BasicStatsSetXMLWriter.java
@@ -2,9 +2,11 @@ package delta.games.lotro.character.stats.base.io.xml;
 
 import javax.xml.transform.sax.TransformerHandler;
 
+import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
+import delta.common.utils.io.xml.XmlWriter;
 import delta.games.lotro.character.stats.BasicStatsSet;
 import delta.games.lotro.character.stats.StatsSetElement;
 import delta.games.lotro.common.stats.StatDescription;
@@ -17,7 +19,7 @@ import delta.games.lotro.utils.NumericUtils;
  */
 public class BasicStatsSetXMLWriter
 {
-  private static final String CDATA="CDATA";
+  private static final Logger LOGGER=Logger.getLogger(BasicStatsSetXMLWriter.class);
 
   /**
    * Write a set of stats to the given XML stream.
@@ -48,27 +50,33 @@ public class BasicStatsSetXMLWriter
     {
       for(StatsSetElement element : statsSet.getStatElements())
       {
+        // Value
+        Number value=element.getValue();
+        String valueStr=NumericUtils.toPersistenceString(value);
+        if (valueStr==null)
+        {
+          LOGGER.warn("Could not persist stat: "+element+" (valueStr is null)");
+          continue;
+        }
         AttributesImpl statAttrs=new AttributesImpl();
         // Stat
         StatDescription stat=element.getStat();
         String key=stat.getPersistenceKey();
-        statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_NAME_ATTR,CDATA,key);
+        statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_NAME_ATTR,XmlWriter.CDATA,key);
         // Operator
         StatOperator operator=element.getOperator();
         if (operator!=StatOperator.ADD)
         {
-          statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_OPERATOR_ATTR,CDATA,operator.name());
+          statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_OPERATOR_ATTR,XmlWriter.CDATA,operator.name());
         }
         // Description override
         String descriptionOverride=element.getDescriptionOverride();
         if ((descriptionOverride!=null) && (descriptionOverride.length()>0))
         {
-          statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_DESCRIPTION_OVERRIDE_ATTR,CDATA,descriptionOverride);
+          statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_DESCRIPTION_OVERRIDE_ATTR,XmlWriter.CDATA,descriptionOverride);
         }
         // Value
-        Number value=element.getValue();
-        String valueStr=NumericUtils.toPersistenceString(value);
-        statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_VALUE_ATTR,CDATA,valueStr);
+        statAttrs.addAttribute("","",BasicStatsSetXMLConstants.STAT_VALUE_ATTR,XmlWriter.CDATA,valueStr);
         hd.startElement("","",BasicStatsSetXMLConstants.STAT_TAG,statAttrs);
         hd.endElement("","",BasicStatsSetXMLConstants.STAT_TAG);
       }
