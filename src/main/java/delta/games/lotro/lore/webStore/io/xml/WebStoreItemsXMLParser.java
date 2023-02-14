f1397ab432c9c8a6c069f06d18f46cb7af71bfd4
Damien Morcellet <dmorcellet@free.fr>
2023-02-14 20:33:29 +0100

    I18n for web store items (issue #397)

--- src/main/java/delta/games/lotro/lore/webStore/io/xml/WebStoreItemsXMLParser.java
+++ src/main/java/delta/games/lotro/lore/webStore/io/xml/WebStoreItemsXMLParser.java
@@ -7,11 +7,13 @@ import java.util.List;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 
+import delta.common.utils.i18n.SingleLocaleLabelsManager;
 import delta.common.utils.xml.DOMParsingTools;
 import delta.games.lotro.common.enums.BillingGroup;
 import delta.games.lotro.common.enums.LotroEnum;
 import delta.games.lotro.common.enums.LotroEnumsRegistry;
 import delta.games.lotro.lore.webStore.WebStoreItem;
+import delta.games.lotro.utils.i18n.I18nFacade;
 
 /**
  * Parser for web store items stored in XML.
@@ -19,6 +21,16 @@ import delta.games.lotro.lore.webStore.WebStoreItem;
  */
 public class WebStoreItemsXMLParser
 {
+  private SingleLocaleLabelsManager _i18n;
+
+  /**
+   * Constructor.
+   */
+  public WebStoreItemsXMLParser()
+  {
+    _i18n=I18nFacade.getLabelsMgr("webStoreItems");
+  }
+
   /**
    * Parse the XML file.
    * @param source Source file.
@@ -35,7 +47,7 @@ public class WebStoreItemsXMLParser
     return ret;
   }
 
-  private static List<WebStoreItem> parseWebStoreItems(Element mainTag)
+  private List<WebStoreItem> parseWebStoreItems(Element mainTag)
   {
     List<WebStoreItem> ret=new ArrayList<WebStoreItem>();
     List<Element> elementTags=DOMParsingTools.getChildTagsByName(mainTag,WebStoreItemsXMLConstants.WEB_STORE_ITEM_TAG,false);
@@ -55,14 +67,14 @@ public class WebStoreItemsXMLParser
    * @param elementTag Tag to read.
    * @return the loaded web store item or <code>null</code>.
    */
-  private static WebStoreItem parseWebStoreItem(Element elementTag)
+  private WebStoreItem parseWebStoreItem(Element elementTag)
   {
     NamedNodeMap attrs=elementTag.getAttributes();
     // Identifier
     int id=DOMParsingTools.getIntAttribute(attrs,WebStoreItemsXMLConstants.WEB_STORE_ITEM_ID_ATTR,0);
     WebStoreItem ret=new WebStoreItem(id);
     // Name
-    String name=DOMParsingTools.getStringAttribute(attrs,WebStoreItemsXMLConstants.WEB_STORE_ITEM_NAME_ATTR,"");
+    String name=_i18n.getLabel(String.valueOf(id));
     ret.setName(name);
     // Free for subscribers
     boolean freeForSubscribers=DOMParsingTools.getBooleanAttribute(attrs,WebStoreItemsXMLConstants.WEB_STORE_ITEM_FREE_FOR_SUBSCRIBERS_ATTR,false);
