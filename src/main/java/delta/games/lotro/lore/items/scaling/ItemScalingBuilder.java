2814790b47187f57d81dcd2a0d5e1f855e32d08a
Damien Morcellet <dmorcellet@free.fr>
2023-08-25 13:19:55 +0200

    Improved the ItemScalingBuilder: remove unsupported cases, support scaling with no regular stats (i.e dps/money only)

--- src/main/java/delta/games/lotro/lore/items/scaling/ItemScalingBuilder.java
+++ src/main/java/delta/games/lotro/lore/items/scaling/ItemScalingBuilder.java
@@ -6,6 +6,8 @@ import delta.games.lotro.common.stats.StatsProvider;
 import delta.games.lotro.config.LotroCoreConfig;
 import delta.games.lotro.lore.items.Item;
 import delta.games.lotro.lore.items.Weapon;
+import delta.games.lotro.lore.items.legendary.Legendary;
+import delta.games.lotro.lore.items.legendary2.Legendary2;
 import delta.games.lotro.utils.maths.Progression;
 
 /**
@@ -22,9 +24,14 @@ public class ItemScalingBuilder
   public static ItemScaling build(Item item)
   {
     ItemScaling ret=new ItemScaling(item);
-    StatsProvider statsProvider=item.getStatsProvider();
-    if (statsProvider==null)
+    if (item instanceof Legendary)
     {
+      // No support for scaling of legendary items
+      return ret;
+    }
+    if (item instanceof Legendary2)
+    {
+      // No support for scaling of new legendary items
       return ret;
     }
     Munging munging=item.getMunging();
@@ -35,6 +42,7 @@ public class ItemScalingBuilder
     Progression progression=munging.getProgression();
     if (progression==null)
     {
+      // TODO Handle this case
       return ret;
     }
     Integer min=munging.getMin();
@@ -43,6 +51,7 @@ public class ItemScalingBuilder
     int levelCap=LotroCoreConfig.getInstance().getMaxCharacterLevel();
     int maxLevel=(max!=null?max.intValue():levelCap);
 
+    StatsProvider statsProvider=item.getStatsProvider();
     for(int level=minLevel;level<=maxLevel;level++)
     {
       Float itemLevel=progression.getValue(level);
@@ -50,13 +59,17 @@ public class ItemScalingBuilder
       {
         continue;
       }
-      BasicStatsSet values=statsProvider.getStats(1,itemLevel.intValue());
+      BasicStatsSet values=null;
+      if (statsProvider!=null)
+      {
+        values=statsProvider.getStats(1,itemLevel.intValue());
+      }
       Money value=item.getValue(itemLevel.intValue());
       Float dps=null;
       if (item instanceof Weapon)
       {
         Weapon weapon=(Weapon)item;
-        dps=weapon.getDPS(itemLevel.intValue());
+        dps=Float.valueOf(weapon.computeDPS(itemLevel.intValue()));
       }
       ItemScalingEntry entry=new ItemScalingEntry(level,itemLevel.intValue(),values,value,dps);
       ret.addEntry(entry);
