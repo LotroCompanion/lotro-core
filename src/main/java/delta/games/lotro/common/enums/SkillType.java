0aa90d0b6fa03e43d4a991e1a49ef0faf30d325f
Damien Morcellet <dmorcellet@free.fr>
2023-09-04 20:41:55 +0200

    Added enum SkillType

---
+++ src/main/java/delta/games/lotro/common/enums/SkillType.java
@@ -0,0 +1,14 @@
+package delta.games.lotro.common.enums;
+
+/**
+ * Skill type.
+ * @author DAM
+ */
+public class SkillType extends LotroEnumEntry
+{
+  @Override
+  public String toString()
+  {
+    return getLabel();
+  }
+}
