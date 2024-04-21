f8c91106b139fe121ac1419ab1c6d08d6e9c6fe8
Damien Morcellet <dmorcellet@free.fr>
2024-04-21 09:18:47 +0200

    Use trait's 'effects at rank' in stats computation

--- src/main/java/delta/games/lotro/character/stats/StatsComputer.java
+++ src/main/java/delta/games/lotro/character/stats/StatsComputer.java
@@ -1,7 +1,10 @@
 package delta.games.lotro.character.stats;
 
 import delta.games.lotro.character.CharacterData;
+import delta.games.lotro.character.traits.EffectAtRank;
 import delta.games.lotro.character.traits.TraitDescription;
+import delta.games.lotro.common.effects.Effect;
+import delta.games.lotro.common.effects.PropertyModificationEffect;
 import delta.games.lotro.common.stats.ScalableStatProvider;
 import delta.games.lotro.common.stats.StatDescription;
 import delta.games.lotro.common.stats.StatProvider;
@@ -24,9 +27,20 @@ public class StatsComputer
   public static BasicStatsSet getStats(CharacterData character, TraitDescription trait, int tier)
   {
     StatsProvider statsProvider=trait.getStatsProvider();
+    int tiersCount=trait.getTiersCount();
+    BasicStatsSet stats=getStats(character,statsProvider,tiersCount,tier);
+    BasicStatsSet statsFromEffects=getStatsFromEffectsAtRank(character,trait,tier);
+    if (statsFromEffects.getStatsCount()>0)
+    {
+      stats.addStats(statsFromEffects);
+    }
+    return stats;
+  }
+
+  private static BasicStatsSet getStats(CharacterData character, StatsProvider statsProvider, int tiersCount, int tier)
+  {
     BasicStatsSet stats=new BasicStatsSet();
     int nbStats=statsProvider.getNumberOfStatProviders();
-    int tiersCount=trait.getTiersCount();
     for(int i=0;i<nbStats;i++)
     {
       StatProvider provider=statsProvider.getStatProvider(i);
@@ -35,6 +49,28 @@ public class StatsComputer
       {
         StatDescription stat=provider.getStat();
         stats.setStat(stat,provider.getOperator(),value,provider.getDescriptionOverride());
+      }
+    }
+    return stats;
+  }
+
+  private static BasicStatsSet getStatsFromEffectsAtRank(CharacterData character, TraitDescription trait, int tier)
+  {
+    BasicStatsSet stats=new BasicStatsSet();
+    for(EffectAtRank effectAtRank : trait.getEffects())
+    {
+      int rank=effectAtRank.getRank();
+      if (rank==tier)
+      {
+        Effect effect=effectAtRank.getEffect();
+        if (effect instanceof PropertyModificationEffect)
+        {
+          PropertyModificationEffect propModEffect=(PropertyModificationEffect)effect;
+          StatsProvider statsProvider=propModEffect.getStatsProvider();
+          int tiersCount=trait.getTiersCount();
+          stats=getStats(character,statsProvider,tiersCount,tier);
+          break;
+        }
       }
     }
     return stats;
