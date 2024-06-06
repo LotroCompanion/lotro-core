77c44fdd5eb01da2056de48cf7e510fc32741ace
Damien Morcellet <dmorcellet@free.fr>
2024-06-06 16:44:36 +0200

    Use new i18n facilities from delta-common

--- src/main/java/delta/games/lotro/utils/i18n/I18nFacade.java
+++ src/main/java/delta/games/lotro/utils/i18n/I18nFacade.java
@@ -2,10 +2,8 @@ package delta.games.lotro.utils.i18n;
 
 import java.io.File;
 
-import org.apache.log4j.Logger;
-
+import delta.common.utils.i18n.LabelsFacade;
 import delta.common.utils.i18n.SingleLocaleLabelsManager;
-import delta.common.utils.i18n.io.xml.LabelsXMLParser;
 import delta.games.lotro.config.DataFiles;
 import delta.games.lotro.config.LotroCoreConfig;
 import delta.games.lotro.config.labels.LabelsConfiguration;
@@ -16,16 +14,27 @@ import delta.games.lotro.config.labels.LabelsConfiguration;
  */
 public class I18nFacade
 {
-  private static final Logger LOGGER=Logger.getLogger(I18nFacade.class);
-
   private static final I18nFacade _instance=new I18nFacade();
 
+  private LabelsFacade _labels;
   private String _localeKey;
 
   private I18nFacade()
   {
     LabelsConfiguration cfg=LotroCoreConfig.getInstance().getLabelsConfiguration();
     _localeKey=cfg.getDataLabelsKey();
+    File rootDir=LotroCoreConfig.getInstance().getFile(DataFiles.LABELS);
+    _labels=new LabelsFacade(rootDir);
+  }
+
+  /**
+   * Get the labels manager for the given key and the current locale.
+   * @param key Set key to use.
+   * @return A labels manager.
+   */
+  private SingleLocaleLabelsManager getLabelsMgrImpl(String key)
+  {
+    return _labels.getLabelsMgr(key,_localeKey);
   }
 
   /**
@@ -35,39 +44,6 @@ public class I18nFacade
    */
   public static SingleLocaleLabelsManager getLabelsMgr(String key)
   {
-    return _instance.buildLabelsMgr(key);
-  }
-
-  private SingleLocaleLabelsManager buildLabelsMgr(String key)
-  {
-    LOGGER.debug("Loading labels file: "+key);
-    SingleLocaleLabelsManager mgr=loadLabelsFile(key,_localeKey);
-    if (mgr==null)
-    {
-      // If no labels file, use the English file
-      mgr=loadLabelsFile(key,"en");
-    }
-    if (mgr==null)
-    {
-      mgr=new SingleLocaleLabelsManager(_localeKey);
-    }
-    return mgr;
-  }
-
-  private SingleLocaleLabelsManager loadLabelsFile(String key, String localeKey)
-  {
-    File rootDir=LotroCoreConfig.getInstance().getFile(DataFiles.LABELS);
-    File labelsDir=new File(rootDir,localeKey);
-    String filename=key+".xml";
-    File from=new File(labelsDir,filename);
-    SingleLocaleLabelsManager mgr=null;
-    if (from.exists())
-    {
-      long now=System.currentTimeMillis();
-      mgr=new LabelsXMLParser().parseSingleLocaleLabels(from);
-      long now2=System.currentTimeMillis();
-      LOGGER.debug("Took: "+(now2-now)+"ms");
-    }
-    return mgr;
+    return _instance.getLabelsMgrImpl(key);
   }
 }
