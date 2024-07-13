c800dd01c3181725968b9b7fd80d6322e86afb2e
Damien Morcellet <dmorcellet@free.fr>
2024-07-13 21:43:07 +0200

    Handle item spellcraft in the gear updater

--- src/main/java/delta/games/lotro/character/utils/CharacterGearUpdater.java
+++ src/main/java/delta/games/lotro/character/utils/CharacterGearUpdater.java
@@ -1,11 +1,16 @@
 package delta.games.lotro.character.utils;
 
+import org.apache.log4j.Logger;
+
 import delta.games.lotro.character.CharacterData;
+import delta.games.lotro.character.CharacterFile;
 import delta.games.lotro.character.gear.CharacterGear;
 import delta.games.lotro.character.gear.GearSlot;
 import delta.games.lotro.character.gear.GearSlotContents;
 import delta.games.lotro.lore.items.Item;
 import delta.games.lotro.lore.items.ItemInstance;
+import delta.games.lotro.lore.items.scaling.ItemSpellcraft;
+import delta.games.lotro.lore.items.scaling.SpellcraftResolver;
 
 /**
  * Character gear updater.
@@ -14,13 +19,28 @@ import delta.games.lotro.lore.items.ItemInstance;
  */
 public class CharacterGearUpdater
 {
+  private static final Logger LOGGER=Logger.getLogger(CharacterGearUpdater.class);
+
+  private CharacterData _data;
+  private SpellcraftResolver _resolver;
+
+  /**
+   * Constructor.
+   * @param toon Character.
+   * @param data Character config.
+   */
+  public CharacterGearUpdater(CharacterFile toon, CharacterData data)
+  {
+    _data=data;
+    _resolver=new SpellcraftResolver(toon,data);
+  }
+
   /**
    * Update the given character data.
-   * @param data Data to update.
    */
-  public void updateGear(CharacterData data)
+  public void updateGear()
   {
-    CharacterGear gear=data.getEquipment();
+    CharacterGear gear=_data.getEquipment();
     for(GearSlot slot : GearSlot.getAll())
     {
       GearSlotContents contents=gear.getSlotContents(slot,false);
@@ -32,11 +52,26 @@ public class CharacterGearUpdater
     }
   }
 
-  private void updateItem(ItemInstance<? extends Item> item)
+  /**
+   * Update the given item.
+   * @param item Item to update.
+   */
+  public void updateItem(ItemInstance<? extends Item> item)
   {
     if (item==null)
     {
       return;
+    }
+    Item reference=item.getReference();
+    ItemSpellcraft spellcraft=reference.getSpellcraft();
+    if (spellcraft!=null)
+    {
+      Integer itemLevel=_resolver.getEffectiveSpellcraft(reference);
+      if (itemLevel!=null)
+      {
+        LOGGER.info("Using spellcraft item level: "+itemLevel);
+        item.setItemLevel(itemLevel);
+      }
     }
     item.updateAutoStats();
   }
