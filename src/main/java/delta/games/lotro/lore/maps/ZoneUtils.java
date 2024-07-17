cd4f8a95a69f43cb03ff7a3c75a59578983c4749
Damien Morcellet <dmorcellet@free.fr>
2024-07-17 17:12:51 +0200

    Added method ZoneUtils.getAreaForBlock from lotro-tools

--- src/main/java/delta/games/lotro/lore/maps/ZoneUtils.java
+++ src/main/java/delta/games/lotro/lore/maps/ZoneUtils.java
@@ -1,5 +1,9 @@
 package delta.games.lotro.lore.maps;
 
+import delta.games.lotro.lore.geo.BlockReference;
+import delta.games.lotro.lore.maps.landblocks.Landblock;
+import delta.games.lotro.lore.maps.landblocks.LandblocksManager;
+
 /**
  * Utility methods related to zones.
  * @author DAM
@@ -24,5 +28,21 @@ public class ZoneUtils
     GeoAreasManager geoAreasManager=GeoAreasManager.getInstance();
     Area area=geoAreasManager.getAreaById(zoneId);
     return area;
+  }
+
+  /**
+   * Get the area for the given block.
+   * @param block Block to use.
+   * @return An area ID or <code>null</code>.
+   */
+  public static Integer getAreaForBlock(BlockReference block)
+  {
+    LandblocksManager mgr=LandblocksManager.getInstance();
+    Landblock lb=mgr.getLandblock(block.getRegion(),block.getBlockX(),block.getBlockY());
+    if (lb!=null)
+    {
+      return lb.getParentArea();
+    }
+    return null;
   }
 }
