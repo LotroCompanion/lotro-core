486ee65127ffb12ac7399e0a3ee0056c54b4c81d
Damien Morcellet <dmorcellet@free.fr>
2024-07-23 22:28:00 +0200

    Added an enum for map note types

---
+++ src/main/java/delta/games/lotro/common/enums/MapNoteType.java
@@ -0,0 +1,14 @@
+package delta.games.lotro.common.enums;
+
+/**
+ * Map note type.
+ * @author DAM
+ */
+public class MapNoteType extends LotroEnumEntry
+{
+  @Override
+  public String toString()
+  {
+    return getLabel();
+  }
+}
