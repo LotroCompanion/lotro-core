8c0c01a2bde961fca9bb7aa7e501e93945972f42
Damien Morcellet <dmorcellet@free.fr>
2024-09-06 22:32:37 +0200

    Added damage qualifiers

---
+++ src/main/java/delta/games/lotro/common/enums/DamageQualifier.java
@@ -0,0 +1,14 @@
+package delta.games.lotro.common.enums;
+
+/**
+ * Damage qualifier.
+ * @author DAM
+ */
+public class DamageQualifier extends LotroEnumEntry
+{
+  @Override
+  public String toString()
+  {
+    return getLabel();
+  }
+}
---
+++ src/main/java/delta/games/lotro/common/enums/DamageQualifiers.java
@@ -0,0 +1,31 @@
+package delta.games.lotro.common.enums;
+
+/**
+ * Well-known damage qualifiers.
+ * @author DAM
+ */
+public class DamageQualifiers
+{
+  /**
+   * Damage qualifier: MELEE.
+   */
+  public static final DamageQualifier MELEE=getByCode(1);
+  /**
+   * Damage qualifier: TACTICAL.
+   */
+  public static final DamageQualifier TACTICAL=getByCode(1);
+  /**
+   * Damage qualifier: RANGED.
+   */
+  public static final DamageQualifier RANGED=getByCode(2);
+
+  /**
+   * Get a damage qualifier using its code.
+   * @param code Code to use.
+   * @return A damage qualifier or <code>null</code>.
+   */
+  public static DamageQualifier getByCode(int code)
+  {
+    return LotroEnumsRegistry.getInstance().get(DamageQualifier.class).getEntry(code);
+  }
+}
