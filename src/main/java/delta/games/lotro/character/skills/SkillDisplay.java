326999b2da445a02d760ce2bfd446100567d6d5e
Damien Morcellet <dmorcellet@free.fr>
2024-10-21 12:25:16 +0200

    Refactoring of skill effects display

--- src/main/java/delta/games/lotro/character/skills/SkillDisplay.java
+++ src/main/java/delta/games/lotro/character/skills/SkillDisplay.java
@@ -13,24 +13,8 @@ import delta.games.lotro.character.skills.attack.SkillAttacks;
 import delta.games.lotro.character.skills.geometry.Arc;
 import delta.games.lotro.character.skills.geometry.Shape;
 import delta.games.lotro.character.skills.geometry.SkillGeometry;
-import delta.games.lotro.common.effects.ApplicationProbability;
-import delta.games.lotro.common.effects.ApplyOverTimeEffect;
-import delta.games.lotro.common.effects.AreaEffect;
-import delta.games.lotro.common.effects.BaseVitalEffect;
-import delta.games.lotro.common.effects.ComboEffect;
 import delta.games.lotro.common.effects.Effect;
-import delta.games.lotro.common.effects.EffectDuration;
-import delta.games.lotro.common.effects.EffectFlags;
-import delta.games.lotro.common.effects.EffectGenerator;
-import delta.games.lotro.common.effects.GenesisEffect;
-import delta.games.lotro.common.effects.Hotspot;
-import delta.games.lotro.common.effects.InduceCombatStateEffect;
-import delta.games.lotro.common.effects.InstantFellowshipEffect;
-import delta.games.lotro.common.effects.PropertyModificationEffect;
-import delta.games.lotro.common.effects.TieredEffect;
-import delta.games.lotro.common.effects.display.EffectDisplay2;
-import delta.games.lotro.common.effects.display.EffectDisplayUtils;
-import delta.games.lotro.common.enums.CombatState;
+import delta.games.lotro.common.effects.display.SkillEffectsDisplay;
 import delta.games.lotro.common.enums.DamageQualifier;
 import delta.games.lotro.common.enums.GambitIconType;
 import delta.games.lotro.common.enums.ImplementUsageType;
@@ -38,12 +22,9 @@ import delta.games.lotro.common.enums.ImplementUsageTypes;
 import delta.games.lotro.common.enums.PipType;
 import delta.games.lotro.common.enums.ResistCategory;
 import delta.games.lotro.common.enums.SkillDisplayType;
-import delta.games.lotro.common.stats.StatUtils;
-import delta.games.lotro.common.stats.StatsProvider;
 import delta.games.lotro.lore.items.DamageType;
 import delta.games.lotro.lore.pip.PipDescription;
 import delta.games.lotro.lore.pip.PipsManager;
-import delta.games.lotro.utils.Proxy;
 
 /**
  * Compute a display of the details of a skill.
@@ -55,6 +36,7 @@ public class SkillDisplay
   private SkillDescription _skill;
   private SkillDetails _skillDetails;
   private SkillAttackComputer _attackComputer;
+  private SkillEffectsDisplay _effectsDisplay;
 
   /**
    * Constructor.
@@ -68,6 +50,7 @@ public class SkillDisplay
     _skill=skill;
     _skillDetails=details;
     _attackComputer=new SkillAttackComputer(data,details);
+    _effectsDisplay=new SkillEffectsDisplay(data,skill,details);
   }
 
   private int getAoEMaxTargets()
@@ -237,6 +220,8 @@ public class SkillDisplay
     }
     List<String> attacks=getAttacksLines();
     table.addAll(attacks);
+    // Effects
+    doSkillEffects(table);
     // Cost
     List<String> costLines=getCostLines();
     table.addAll(costLines);
@@ -329,7 +314,6 @@ public class SkillDisplay
       }
       doEffects(attack,ret);
     }
-    doSkillEffects(ret);
     return ret;
   }
 
@@ -366,157 +350,7 @@ public class SkillDisplay
 
   private void handleEffect(DamageQualifier damageQualifier, SkillEffectGenerator generator, Effect effect, List<String> storage)
   {
-    // Check probability
-    boolean applicable=checkEffectApplicationProbability(effect);
-    if (!applicable)
-    {
-      return;
-    }
-    String description=effect.getDescription();
-    if (!description.isEmpty())
-    {
-      storage.add(description);
-    }
-    if (effect instanceof BaseVitalEffect)
-    {
-      BaseVitalEffect vitalEffect=(BaseVitalEffect)effect;
-      EffectDisplay2 d2=new EffectDisplay2(_character,_skillDetails);
-      String display=d2.getVitalEffectDisplay(generator,vitalEffect,damageQualifier);
-      storage.add(display);
-    }
-    else if (effect instanceof ComboEffect)
-    {
-      ComboEffect comboEffect=(ComboEffect)effect;
-      Proxy<Effect> toExamine=comboEffect.getToExamine();
-      if (toExamine!=null)
-      {
-        handleEffect(damageQualifier,generator,toExamine.getObject(),storage);
-      }
-    }
-    else if (effect instanceof GenesisEffect)
-    {
-      GenesisEffect genesisEffect=(GenesisEffect)effect;
-      Hotspot hotspot=genesisEffect.getHotspot();
-      if (hotspot!=null)
-      {
-        for(EffectGenerator hotspotGenerator : hotspot.getEffects())
-        {
-          handleEffect(damageQualifier,generator,hotspotGenerator.getEffect(),storage);
-        }
-      }
-    }
-    else if (effect instanceof AreaEffect)
-    {
-      AreaEffect areaEffect=(AreaEffect)effect;
-      float range=areaEffect.getRange();
-      if (!areaEffect.getEffects().isEmpty())
-      {
-        String line="Effects applied to enemies within "+L10n.getString(range,0)+" metres:";
-        storage.add(line);
-        for(EffectGenerator childGenerator : areaEffect.getEffects())
-        {
-          handleEffect(damageQualifier,generator,childGenerator.getEffect(),storage);
-        }
-      }
-    }
-    else if (effect instanceof ApplyOverTimeEffect)
-    {
-      ApplyOverTimeEffect applyOverTimeEffect=(ApplyOverTimeEffect)effect;
-      if (!applyOverTimeEffect.getAppliedEffects().isEmpty())
-      {
-        float interval=applyOverTimeEffect.getInterval();
-        String seconds=(interval>1.0f)?" seconds:":" second:";
-        String line="Every "+L10n.getString(interval,1)+seconds;
-        storage.add(line);
-        for(EffectGenerator childGenerator : applyOverTimeEffect.getAppliedEffects())
-        {
-          handleEffect(damageQualifier,generator,childGenerator.getEffect(),storage);
-        }
-      }
-    }
-    else if (effect instanceof PropertyModificationEffect)
-    {
-      PropertyModificationEffect propModEffect=(PropertyModificationEffect)effect;
-      StatsProvider provider=propModEffect.getStatsProvider();
-      if (provider!=null)
-      {
-        int level=_character.getLevel();
-        List<String> lines=StatUtils.getFullStatsForDisplay(provider,level);
-        storage.addAll(lines);
-      }
-      boolean expiresOutOfCombat=effect.getBaseFlag(EffectFlags.DURATION_COMBAT_ONLY);
-      if (expiresOutOfCombat)
-      {
-        // TODO Sometimes "Expires if out of combat for a short amount of time."
-        storage.add("Expires if out of combat for 9 seconds.");
-      }
-      EffectDuration effectDuration=propModEffect.getEffectDuration();
-      if (effectDuration!=null)
-      {
-        Float duration=effectDuration.getDuration();
-        if (duration!=null)
-        {
-          String line="Duration: "+L10n.getString(duration.doubleValue(),1)+"s";
-          storage.add(line);
-        }
-      }
-    }
-    else if (effect instanceof TieredEffect)
-    {
-      TieredEffect propModEffect=(TieredEffect)effect;
-      EffectGenerator firstTier=propModEffect.getTiers().get(0);
-      handleEffect(damageQualifier,generator,firstTier.getEffect(),storage);
-    }
-    else if (effect instanceof InstantFellowshipEffect)
-    {
-      InstantFellowshipEffect fellowshipEffect=(InstantFellowshipEffect)effect;
-      Float range=fellowshipEffect.getRange();
-      boolean toPets=fellowshipEffect.appliesToPets();
-      if (toPets)
-      {
-        String line="Effects applied to your animal companion";
-        if (range!=null)
-        {
-          line=line+" within "+L10n.getString(range.doubleValue(),0)+" metres:";
-        }
-        storage.add(line);
-      }
-      for(EffectGenerator childEffect : fellowshipEffect.getEffects())
-      {
-        handleEffect(damageQualifier,generator,childEffect.getEffect(),storage);
-      }
-    }
-    else if (effect instanceof InduceCombatStateEffect)
-    {
-      showInduceCombatStateEffect(storage,(InduceCombatStateEffect)effect);
-    }
-  }
-
-  private void showInduceCombatStateEffect(List<String> storage, InduceCombatStateEffect effect)
-  {
-    float duration=effect.getDuration();
-    duration+=_character.computeAdditiveModifiers(effect.getDurationModifiers());
-    CombatState state=effect.getCombatState();
-    String stateStr="?";
-    if (state!=null)
-    {
-      stateStr=EffectDisplayUtils.getStateLabel(state);
-    }
-    String text=L10n.getString(duration,1)+"s "+stateStr;
-    storage.add(text);
-  }
-
-  private boolean checkEffectApplicationProbability(Effect effect)
-  {
-    ApplicationProbability probability=effect.getApplicationProbability();
-    if (probability==ApplicationProbability.ALWAYS)
-    {
-      return true;
-    }
-    float probabilityValue=probability.getProbability();
-    Integer modifier=probability.getModProperty();
-    probabilityValue+=_character.computeAdditiveModifier(modifier);
-    return (probabilityValue>0);
+    _effectsDisplay.handleEffect(damageQualifier,generator,effect,storage);
   }
 
   private List<String> getCostLines()
---
+++ src/main/java/delta/games/lotro/common/effects/display/SkillEffectsDisplay.java
@@ -0,0 +1,213 @@
+package delta.games.lotro.common.effects.display;
+
+import java.util.List;
+
+import delta.common.utils.l10n.L10n;
+import delta.games.lotro.character.skills.SkillDescription;
+import delta.games.lotro.character.skills.SkillDetails;
+import delta.games.lotro.character.skills.SkillEffectGenerator;
+import delta.games.lotro.character.skills.attack.CharacterDataForSkills;
+import delta.games.lotro.common.effects.ApplicationProbability;
+import delta.games.lotro.common.effects.ApplyOverTimeEffect;
+import delta.games.lotro.common.effects.AreaEffect;
+import delta.games.lotro.common.effects.BaseVitalEffect;
+import delta.games.lotro.common.effects.ComboEffect;
+import delta.games.lotro.common.effects.Effect;
+import delta.games.lotro.common.effects.EffectDuration;
+import delta.games.lotro.common.effects.EffectFlags;
+import delta.games.lotro.common.effects.EffectGenerator;
+import delta.games.lotro.common.effects.GenesisEffect;
+import delta.games.lotro.common.effects.Hotspot;
+import delta.games.lotro.common.effects.InduceCombatStateEffect;
+import delta.games.lotro.common.effects.InstantFellowshipEffect;
+import delta.games.lotro.common.effects.PropertyModificationEffect;
+import delta.games.lotro.common.effects.TieredEffect;
+import delta.games.lotro.common.enums.CombatState;
+import delta.games.lotro.common.enums.DamageQualifier;
+import delta.games.lotro.common.stats.StatUtils;
+import delta.games.lotro.common.stats.StatsProvider;
+import delta.games.lotro.utils.Proxy;
+
+/**
+ * Facilities to display skill effects.
+ * @author DAM
+ */
+public class SkillEffectsDisplay
+{
+  private CharacterDataForSkills _character;
+  private SkillDetails _skillDetails;
+
+  /**
+   * Constructor.
+   * @param data Access to character data related to skills.
+   * @param skill Skill.
+   * @param details Skill details.
+   */
+  public SkillEffectsDisplay(CharacterDataForSkills data, SkillDescription skill, SkillDetails details)
+  {
+    _character=data;
+    _skillDetails=details;
+  }
+
+  /**
+   * Handle a skill effect.
+   * @param damageQualifier Damage qualifier.
+   * @param generator Effect generator.
+   * @param effect Effect.
+   * @param storage Storage for generated text.
+   */
+  public void handleEffect(DamageQualifier damageQualifier, SkillEffectGenerator generator, Effect effect, List<String> storage)
+  {
+    // Check probability
+    boolean applicable=checkEffectApplicationProbability(effect);
+    if (!applicable)
+    {
+      return;
+    }
+    String description=effect.getDescription();
+    if (!description.isEmpty())
+    {
+      storage.add(description);
+    }
+    if (effect instanceof BaseVitalEffect)
+    {
+      BaseVitalEffect vitalEffect=(BaseVitalEffect)effect;
+      EffectDisplay2 d2=new EffectDisplay2(_character,_skillDetails);
+      String display=d2.getVitalEffectDisplay(generator,vitalEffect,damageQualifier);
+      storage.add(display);
+    }
+    else if (effect instanceof ComboEffect)
+    {
+      ComboEffect comboEffect=(ComboEffect)effect;
+      Proxy<Effect> toExamine=comboEffect.getToExamine();
+      if (toExamine!=null)
+      {
+        handleEffect(damageQualifier,generator,toExamine.getObject(),storage);
+      }
+    }
+    else if (effect instanceof GenesisEffect)
+    {
+      GenesisEffect genesisEffect=(GenesisEffect)effect;
+      Hotspot hotspot=genesisEffect.getHotspot();
+      if (hotspot!=null)
+      {
+        for(EffectGenerator hotspotGenerator : hotspot.getEffects())
+        {
+          handleEffect(damageQualifier,generator,hotspotGenerator.getEffect(),storage);
+        }
+      }
+    }
+    else if (effect instanceof AreaEffect)
+    {
+      AreaEffect areaEffect=(AreaEffect)effect;
+      float range=areaEffect.getRange();
+      if (!areaEffect.getEffects().isEmpty())
+      {
+        String line="Effects applied to enemies within "+L10n.getString(range,0)+" metres:";
+        storage.add(line);
+        for(EffectGenerator childGenerator : areaEffect.getEffects())
+        {
+          handleEffect(damageQualifier,generator,childGenerator.getEffect(),storage);
+        }
+      }
+    }
+    else if (effect instanceof ApplyOverTimeEffect)
+    {
+      ApplyOverTimeEffect applyOverTimeEffect=(ApplyOverTimeEffect)effect;
+      if (!applyOverTimeEffect.getAppliedEffects().isEmpty())
+      {
+        float interval=applyOverTimeEffect.getInterval();
+        String seconds=(interval>1.0f)?" seconds:":" second:";
+        String line="Every "+L10n.getString(interval,1)+seconds;
+        storage.add(line);
+        for(EffectGenerator childGenerator : applyOverTimeEffect.getAppliedEffects())
+        {
+          handleEffect(damageQualifier,generator,childGenerator.getEffect(),storage);
+        }
+      }
+    }
+    else if (effect instanceof PropertyModificationEffect)
+    {
+      PropertyModificationEffect propModEffect=(PropertyModificationEffect)effect;
+      StatsProvider provider=propModEffect.getStatsProvider();
+      if (provider!=null)
+      {
+        int level=_character.getLevel();
+        List<String> lines=StatUtils.getFullStatsForDisplay(provider,level);
+        storage.addAll(lines);
+      }
+      boolean expiresOutOfCombat=effect.getBaseFlag(EffectFlags.DURATION_COMBAT_ONLY);
+      if (expiresOutOfCombat)
+      {
+        // TODO Sometimes "Expires if out of combat for a short amount of time."
+        storage.add("Expires if out of combat for 9 seconds.");
+      }
+      EffectDuration effectDuration=propModEffect.getEffectDuration();
+      if (effectDuration!=null)
+      {
+        Float duration=effectDuration.getDuration();
+        if (duration!=null)
+        {
+          String line="Duration: "+L10n.getString(duration.doubleValue(),1)+"s";
+          storage.add(line);
+        }
+      }
+    }
+    else if (effect instanceof TieredEffect)
+    {
+      TieredEffect propModEffect=(TieredEffect)effect;
+      EffectGenerator firstTier=propModEffect.getTiers().get(0);
+      handleEffect(damageQualifier,generator,firstTier.getEffect(),storage);
+    }
+    else if (effect instanceof InstantFellowshipEffect)
+    {
+      InstantFellowshipEffect fellowshipEffect=(InstantFellowshipEffect)effect;
+      Float range=fellowshipEffect.getRange();
+      boolean toPets=fellowshipEffect.appliesToPets();
+      if (toPets)
+      {
+        String line="Effects applied to your animal companion";
+        if (range!=null)
+        {
+          line=line+" within "+L10n.getString(range.doubleValue(),0)+" metres:";
+        }
+        storage.add(line);
+      }
+      for(EffectGenerator childEffect : fellowshipEffect.getEffects())
+      {
+        handleEffect(damageQualifier,generator,childEffect.getEffect(),storage);
+      }
+    }
+    else if (effect instanceof InduceCombatStateEffect)
+    {
+      showInduceCombatStateEffect(storage,(InduceCombatStateEffect)effect);
+    }
+  }
+
+  private void showInduceCombatStateEffect(List<String> storage, InduceCombatStateEffect effect)
+  {
+    float duration=effect.getDuration();
+    duration+=_character.computeAdditiveModifiers(effect.getDurationModifiers());
+    CombatState state=effect.getCombatState();
+    String stateStr="?";
+    if (state!=null)
+    {
+      stateStr=EffectDisplayUtils.getStateLabel(state);
+    }
+    String text=L10n.getString(duration,1)+"s "+stateStr;
+    storage.add(text);
+  }
+
+  private boolean checkEffectApplicationProbability(Effect effect)
+  {
+    ApplicationProbability probability=effect.getApplicationProbability();
+    if (probability==ApplicationProbability.ALWAYS)
+    {
+      return true;
+    }
+    float probabilityValue=probability.getProbability();
+    Integer modifier=probability.getModProperty();
+    probabilityValue+=_character.computeAdditiveModifier(modifier);
+    return (probabilityValue>0);
+  }
+}
