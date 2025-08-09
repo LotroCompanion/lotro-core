4d8562fa53663f5445df93d1a84ceaf7430de57d
Damien Morcellet <dmorcellet@free.fr>
2025-08-09 22:59:09 +0200

    Added a tool method to get a list of CharacterFile from a list of toon identifiers

--- src/main/java/delta/games/lotro/character/utils/CharacterUtils.java
+++ src/main/java/delta/games/lotro/character/utils/CharacterUtils.java
@@ -1,9 +1,15 @@
 package delta.games.lotro.character.utils;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import delta.games.lotro.account.Account;
 import delta.games.lotro.account.AccountReference;
 import delta.games.lotro.account.AccountsManager;
 import delta.games.lotro.character.CharacterFile;
+import delta.games.lotro.character.CharactersManager;
 
 /**
  * Utility methods related to characters management.
@@ -30,5 +36,33 @@ public class CharacterUtils
       account=accountsMgr.getAccountByID(accountID);
     }
     return account;
+  }
+
+  /**
+   * Build a list of toons from a list of character IDs.
+   * @param toonIds Input IDs.
+   * @return A list of unique toons, possibly empty but never <code>null</code>.
+   */
+  public static List<CharacterFile> getToons(List<String> toonIds)
+  {
+    List<CharacterFile> toons=new ArrayList<CharacterFile>();
+    CharactersManager manager=CharactersManager.getInstance();
+    if ((toonIds!=null) && (!toonIds.isEmpty()))
+    {
+      Set<String> addedToons=new HashSet<String>();
+      for(String toonID : toonIds)
+      {
+        if (!addedToons.contains(toonID))
+        {
+          CharacterFile toon=manager.getToonById(toonID);
+          if (toon!=null)
+          {
+            toons.add(toon);
+          }
+          addedToons.add(toonID);
+        }
+      }
+    }
+    return toons;
   }
 }
