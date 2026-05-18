86525456f4701fe13588774ab99240a03c1f477e
Damien Morcellet <dmorcellet@free.fr>
2026-05-18 22:51:56 +0200

    Added POJOs for achievable loots (issue LotroCompanion/lotro-companion#706)

---
+++ src/main/java/delta/games/lotro/lore/quests/loots/AchievableLoot.java
@@ -0,0 +1,123 @@
+package delta.games.lotro.lore.quests.loots;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import delta.games.lotro.lore.agents.mobs.MobDescription;
+import delta.games.lotro.lore.items.Item;
+
+/**
+ * Elementary loot specification for an achievable.
+ * @author DAM
+ */
+public class AchievableLoot
+{
+  private Item _item;
+  private int[] _probabilities;
+  // Single mob
+  private MobDescription _mob;
+  // Or mob specs
+  private List<AchievableLootMonsterSpec> _monsterSpecs;
+  private List<AchievableLootMonsterSpec> _excludedMonsterSpecs;
+
+  /**
+   * Constructor.
+   * @param item Targeted item.
+   */
+  public AchievableLoot(Item item)
+  {
+    _item=item;
+    _monsterSpecs=null;
+    _mob=null;
+    _excludedMonsterSpecs=null;
+  }
+
+  /**
+   * Get the targeted item.
+   * @return an item.
+   */
+  public Item getItem()
+  {
+    return _item;
+  }
+
+  /**
+   * Get the loot probabilities.
+   * @return an array of probabilities.
+   */
+  public int[] getProbabilities()
+  {
+    return _probabilities;
+  }
+
+  /**
+   * Set the loot probabilities.
+   * @param probabilities Probabilities to set.
+   */
+  public void setProbabilities(int[] probabilities)
+  {
+    _probabilities=probabilities;
+  }
+
+  /**
+   * Get the targeted mob.
+   * @return a mob or <code>null</code>.
+   */
+  public MobDescription getMob()
+  {
+    return _mob;
+  }
+
+  /**
+   * Set the targeted mob.
+   * @param mob Mob to set.
+   */
+  public void setMob(MobDescription mob)
+  {
+    _mob=mob;
+  }
+
+  /**
+   * Get the monster specifications.
+   * @return A non-empty list of such specifications, or <code>null</code>
+   */
+  public List<AchievableLootMonsterSpec> getMonsterSpecs()
+  {
+    return _monsterSpecs;
+  }
+
+  /**
+   * Add a monster specification.
+   * @param monsterSpec Specification to add.
+   */
+  public void addMonsterSpec(AchievableLootMonsterSpec monsterSpec)
+  {
+    if (_monsterSpecs==null)
+    {
+      _monsterSpecs=new ArrayList<AchievableLootMonsterSpec>();
+    }
+    _monsterSpecs.add(monsterSpec);
+  }
+
+  /**
+   * Get the monster specifications to exclude.
+   * @return A non-empty list of such specifications, or <code>null</code>
+   */
+  public List<AchievableLootMonsterSpec> getExcludedMonsterSpecs()
+  {
+    return _excludedMonsterSpecs;
+  }
+
+  /**
+   * Add a monster specification to exclude.
+   * @param excludedMonsterSpec Specification to add.
+   */
+  public void addExcludedMonsterSpec(AchievableLootMonsterSpec excludedMonsterSpec)
+  {
+    if (_excludedMonsterSpecs==null)
+    {
+      _excludedMonsterSpecs=new ArrayList<AchievableLootMonsterSpec>();
+    }
+    _excludedMonsterSpecs.add(excludedMonsterSpec);
+  }
+}
